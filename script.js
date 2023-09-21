79e71e3dc37b4b7765e41b39c9c4afd42bd8cf75
DmitriyAngve <kirirch129@gmail.com>
2023-09-21 13:30:53 +0700

    vowel2index

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ Given an item at $75 sale price after applying a 25% discount, the function shou
 DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price
 */
 // SOLUTION
-
+/*
 function discoverOriginalPrice(discountedPrice, salePercentage) {
   const discount = salePercentage / 100;
   const original = discountedPrice / (1 - discount);
@@ -17,10 +17,23 @@ function discoverOriginalPrice(discountedPrice, salePercentage) {
 console.log(discoverOriginalPrice(75, 25));
 console.log(discoverOriginalPrice(75.75, 25));
 console.log(discoverOriginalPrice(25, 75));
-
+*/
 // #2
 /*
-
+Write a function
+vowel2index(str)
+that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+E.g:
+vowel2index('this is my string') == 'th3s 6s my str15ng'
+vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel2index('') == ''
+Your function should be case insensitive to the vowels.
 */
+
+function vowel2index(str) {
+  return str.replace(/[aeiou]/gi, (m, i) => i + 1);
+}
+
+console.log(vowel2index("this is my string"));
 
 // SOLUTION
