61f2c109ce72d5998f7487d503a41287c96c475c
DmitriyAngve <kirirch129@gmail.com>
2023-10-12 14:13:05 +0700

    findMissingLetter

--- script.js
+++ script.js
@@ -551,6 +551,7 @@ Examples:
 * 'abc' =>  ['ab', 'c_']
 * 'abcdef' => ['ab', 'cd', 'ef']
 */
+/*
 function solution(str) {
   const pairs = [];
   for (let i = 0; i < str.length; i += 2) {
@@ -567,3 +568,31 @@ function solution(str) {
 console.log(solution("abcdef"));
 console.log(solution("abcdefg"));
 console.log(solution(""));
+*/
+
+// #6
+/*
+Find the missing letter
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+Example:
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+I have also created other katas. Take a look if you enjoyed this kata!
+*/
+function findMissingLetter(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    const current = arr[i].charCodeAt();
+    const next = arr[i + 1].charCodeAt();
+
+    if (next - current > 1) {
+      return String.fromCharCode(current + 1);
+    }
+  }
+}
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
+console.log(findMissingLetter(["O", "Q", "R", "S"]));
