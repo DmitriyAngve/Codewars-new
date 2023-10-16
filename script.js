c0360052b2d0a4406b8324096989d52da6b81fb5
DmitriyAngve <kirirch129@gmail.com>
2023-10-16 18:57:18 +0700

    breakChocolate

--- script.js
+++ script.js
@@ -769,6 +769,7 @@ The result may contain fives. ;-)
 The start number will always be smaller than the end number. Both numbers can be also negative!
 I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
 */
+/*
 function dontGiveMeFive(start, end) {
   let count = 0;
   for (let i = start; i <= end; i++) {
@@ -781,3 +782,22 @@ function dontGiveMeFive(start, end) {
 }
 
 console.log(dontGiveMeFive(1, 81));
+*/
+
+// #6
+/*
+Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
+If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+*/
+function breakChocolate(n, m) {
+  if (typeof n !== "number" || typeof m !== "number") {
+    return 0;
+  }
+
+  if (n <= 0 || m <= 0) return 0;
+
+  return n * m - 1;
+}
+
+console.log(breakChocolate(5, 5));
