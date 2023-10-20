f17f9d19c87dd23e98192bf1a420a8903fefafa7
DmitriyAngve <kirirch129@gmail.com>
2023-10-20 12:57:34 +0700

    fizzbuzz

--- script.js
+++ script.js
@@ -1276,7 +1276,7 @@ Examples
 "#######"       ==>  ""
 ""              ==>  ""
 */
-
+/*
 function cleanString(s) {
   if (!s.length) return "";
   let arr = s.split("");
@@ -1295,3 +1295,37 @@ function cleanString(s) {
 console.log(cleanString("abc#d##c"));
 console.log(cleanString("abc##d######"));
 console.log(cleanString(""));
+*/
+
+// 20.10.2023
+/*
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+Replace certain values however if any of the following conditions are met:
+    If the value is a multiple of 3: use the value "Fizz" instead
+    If the value is a multiple of 5: use the value "Buzz" instead
+    If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+Method calling example:
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+*/
+
+function fizzbuzz(n) {
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(fizzbuzz(3));
+console.log(fizzbuzz(4));
+console.log(fizzbuzz(15));
