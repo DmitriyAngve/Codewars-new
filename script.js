57c1eefe872de9f37be1369effbbbb3486a468d4
DmitriyAngve <kirirch129@gmail.com>
2023-10-20 14:13:51 +0700

    isLeapYear

--- script.js
+++ script.js
@@ -1391,6 +1391,7 @@ Examples
 -456 -> -654
 1000 ->    1
 */
+/*
 function reverseNumber(n) {
   const arr = [...(n + "")];
 
@@ -1410,3 +1411,30 @@ function reverseNumber(n) {
 console.log(reverseNumber(123));
 console.log(reverseNumber(1000));
 console.log(reverseNumber(-456));
+*/
+
+// #5
+/*
+In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+    years divisible by 4 are leap years
+    but years divisible by 100 are not leap years
+    but years divisible by 400 are leap years
+Additional Notes:
+    Only valid years (positive integers) will be tested, so you don't have to validate them
+Examples can be found in the test fixture.
+*/
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(isLeapYear(2020));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(2015));
+console.log(isLeapYear(2100));
