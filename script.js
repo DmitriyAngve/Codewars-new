993b7857b693aebd5fcd23044ac6ea1677a485c7
DmitriyAngve <kirirch129@gmail.com>
2023-10-25 17:44:20 +0700

    cookingTime

--- script.js
+++ script.js
@@ -2157,6 +2157,7 @@ your function should return the following array:
   } 
 ];
 */
+/*
 function greetDevelopers(list) {
   return list.map(function (developer) {
     developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
@@ -2193,3 +2194,31 @@ console.log(
     },
   ])
 );
+*/
+
+// #8
+/*
+You are the greatest chef on earth. No one boils eggs like you! Your restaurant is always full of guests, who love your boiled eggs. But when there is a greater order of boiled eggs, you need some time, because you have only one pot for your job. How much time do you need?
+Your Task
+Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
+Rules
+    you can put at most 8 eggs into the pot at once
+    it takes 5 minutes to boil an egg
+    we assume, that the water is boiling all the time (no time to heat up)
+    for simplicity we also don't consider the time it takes to put eggs into the pot or get them out of it
+Example (Input --> Output)
+0 --> 0
+5 --> 5
+10 --> 10
+*/
+function cookingTime(eggs) {
+  const batches = Math.ceil(eggs / 8);
+
+  const total = batches * 5;
+  return total;
+}
+
+console.log(cookingTime(10));
+console.log(cookingTime(16));
+console.log(cookingTime(5));
+console.log(cookingTime(0));
