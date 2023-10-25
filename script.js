559425a204f537a3f4a20d3f24b7a6f14d348b8a
DmitriyAngve <kirirch129@gmail.com>
2023-10-25 16:09:50 +0700

    score

--- script.js
+++ script.js
@@ -1925,6 +1925,7 @@ When two numbers have the same "weight", let us class them as if they were strin
 180 is before 90 since, having the same "weight" (9), it comes before as a string.
 All numbers in the list are positive numbers and the list can be empty.
 */
+/*
 function orderWeight(str) {
   const arr = str.split(" ");
 
@@ -1955,3 +1956,59 @@ function sum(el) {
 
 console.log(orderWeight("103 123 4444 99 2000"));
 console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+*/
+
+// #2
+/*
+Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+ Three 1's => 1000 points
+ Three 6's =>  600 points
+ Three 5's =>  500 points
+ Three 4's =>  400 points
+ Three 3's =>  300 points
+ Three 2's =>  200 points
+ One   1   =>  100 points
+ One   5   =>   50 point
+A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+Example scoring
+ Throw       Score
+ ---------   ------------------
+ 5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+ 1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+ 2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+*/
+function score(dice) {
+  // Создаю массив длиной 7 (для значений от 1 до 6) и заполняю нулями. Массив для подсчета количества каждого значения на костях
+  const counts = new Array(7).fill(0);
+  console.log(counts);
+  // Перебираю каждое значение в массиве dice с помощью forEach. Для каждого значения увеличиваю соответствующий счётчик в массиве counts (Если в dice есть кость со значением 3, то увеличиваю count[3] на 1)
+  dice.forEach((value) => {
+    counts[value]++;
+  });
+
+  // Переменная для хранения общего количества баллов
+  let totalScore = 0;
+
+  // Начинаю обработку 3-ек. Использую цикл, чтобы перебрать все возможные значения на костях от value=1 до value<=6
+  for (let value = 1; value <= 6; value++) {
+    // Добавляю проверку, есть ли хотя бы три кости с текущим знаением value.
+    if (counts[value] >= 3) {
+      // Если есть, то выполнется добавление 1000 очков
+      if (value === 1) {
+        totalScore += 1000;
+        // В противном случае, добавляю 100 очков умноженных на value (количество троек)
+      } else {
+        totalScore += value * 100;
+      }
+      // Затем уменьшаю счётчик этого значения в counts на 3
+      counts[value] -= 3;
+    }
+
+    // Обработка отдельных единиц и пятерок
+  }
+  totalScore += counts[1] * 100;
+  totalScore += counts[5] * 50;
+  return totalScore;
+}
+
+console.log(score([2, 4, 4, 5, 4]));
