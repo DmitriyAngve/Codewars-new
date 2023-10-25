71b4ec4bad08c51349dd976341ff35684a1328cb
DmitriyAngve <kirirch129@gmail.com>
2023-10-25 17:32:47 +0700

    greetDevelopers

--- script.js
+++ script.js
@@ -2119,6 +2119,7 @@ Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, T
 If there are less than 2 blue beads return 0.
 Fundamentals
 */
+/*
 function countRedBeads(n) {
   if (n < 2) {
     return 0;
@@ -2130,3 +2131,65 @@ function countRedBeads(n) {
 console.log(countRedBeads(1));
 console.log(countRedBeads(3));
 console.log(countRedBeads(5));
+*/
+
+// #7
+/*
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+Hi < firstName here >, what do you like the most about < language here >?
+For example, given the following input array:
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+your function should return the following array:
+[
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+    greeting: 'Hi Madison, what do you like the most about Ruby?'
+  } 
+];
+*/
+function greetDevelopers(list) {
+  return list.map(function (developer) {
+    developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
+
+    return developer;
+  });
+}
+
+console.log(
+  greetDevelopers([
+    {
+      firstName: "Sofia",
+      lastName: "I.",
+      country: "Argentina",
+      continent: "Americas",
+      age: 35,
+      language: "Java",
+    },
+    {
+      firstName: "Lukas",
+      lastName: "X.",
+      country: "Croatia",
+      continent: "Europe",
+      age: 35,
+      language: "Python",
+    },
+    {
+      firstName: "Madison",
+      lastName: "U.",
+      country: "United States",
+      continent: "Americas",
+      age: 32,
+      language: "Ruby",
+    },
+  ])
+);
