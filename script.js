a0a6a790a1acca7f3881f7cb081d4dfbbab9c99a
DmitriyAngve <kirirch129@gmail.com>
2023-10-29 16:30:41 +0700

    arrincludes

--- script.js
+++ script.js
@@ -2538,6 +2538,7 @@ Given a string of words (x), you need to return an array of the words, sorted al
 If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
 All inputs will be valid.
 */
+/*
 function last(x) {
   return x
     .split(" ")
@@ -2545,3 +2546,26 @@ function last(x) {
 }
 
 console.log(last("man i need a taxi up to ubud"));
+*/
+
+// 29.10.2023
+
+// #1
+/*
+In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+Examples:
+[1, -1, 2, -2, 3] => 3
+3 has no matching negative appearance
+[-3, 1, 2, 3, -1, -4, -2] => -4
+-4 has no matching positive appearance
+[1, -1, 2, -2, 3, 3] => 3
+(the only-positive or only-negative integer may appear more than once)
+Good luck!
+*/
+function solve(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr.includes(-1 * arr[i])) return arr[i];
+  }
+}
+
+console.log(solve([1, -1, 2, -2, 3]));
