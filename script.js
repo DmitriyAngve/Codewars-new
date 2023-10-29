b7770c938e7414143ed51b0611a3668196d602dd
DmitriyAngve <kirirch129@gmail.com>
2023-10-29 16:46:01 +0700

    incrementer

--- script.js
+++ script.js
@@ -2562,6 +2562,7 @@ Examples:
 (the only-positive or only-negative integer may appear more than once)
 Good luck!
 */
+/*
 function solve(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (!arr.includes(-1 * arr[i])) return arr[i];
@@ -2569,3 +2570,31 @@ function solve(arr) {
 }
 
 console.log(solve([1, -1, 2, -2, 3]));
+*/
+
+// #2
+/*
+Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+Notes:
+    return an empty array if your array is empty
+    arrays will only contain numbers so don't worry about checking that
+Examples:
+[1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+[4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
+                                       #  9+3 = 12  -->  2
+*/
+function incrementer(nums) {
+  let result = [];
+  if (!nums.length) return [];
+  for (let i = 0; i < nums.length; i++) {
+    let res = nums[i] + i + 1;
+
+    result.push(res % 10);
+  }
+
+  return result;
+}
+
+console.log(incrementer([4, 6, 9, 1, 3]));
+console.log(incrementer([3, 6, 9, 8, 9]));
