bbda426ec046ab0c48506672190428b88499a2c1
DmitriyAngve <kirirch129@gmail.com>
2023-11-13 12:37:31 +0700

    pattern

--- script.js
+++ script.js
@@ -3261,6 +3261,7 @@ Depending on the cumulative score of the team, return the appropriate sentiment:
 < 100 & > 80: 'i can handle this'
 100 or over: 'party time!!'
 */
+/*
 function boredom(staff) {
   let boredomScores = {
     accounts: 1,
@@ -3306,3 +3307,59 @@ console.log(
     mr: "finance",
   })
 );
+*/
+
+// #2
+/*
+You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+    Note:Returning the pattern is not the same as Printing the pattern.
+Rules/Note:
+    If n < 1 then it should return "" i.e. empty string.
+    There are no whitespaces in the pattern.
+Pattern:
+1
+22
+333
+....
+.....
+nnnnnn
+Examples:
+    pattern(5):
+    1
+    22
+    333
+    4444
+    55555
+    pattern(11):
+    1
+    22
+    333
+    4444
+    55555
+    666666
+    7777777
+    88888888
+    999999999
+    10101010101010101010
+    1111111111111111111111
+    Hint: Use \n in string to jump to next line
+*/
+function pattern(n) {
+  if (n < 1) {
+    return "";
+  }
+
+  let result = "";
+
+  for (let i = 1; i <= n; i++) {
+    result += `${i}`.repeat(i);
+    if (i < n) {
+      result += "\n";
+    }
+  }
+  return result;
+}
+
+console.log(pattern(1));
+console.log(pattern(2));
+console.log(pattern(5));
