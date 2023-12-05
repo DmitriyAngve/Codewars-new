abac33465d276f9e464738c7ffef3b68278766cb
DmitriyAngve <kirirch129@gmail.com>
2023-12-05 14:52:35 +0300

    sumOfN

--- script.js
+++ script.js
@@ -3790,6 +3790,7 @@ Notes
     All numbers are valid.
     There can be duplicate letters and numbers.
 */
+/*
 function lastSurvivor(letters, coords) {
   // FIRST
   // for (let i = 0; i < coords.length; i++) {
@@ -3809,3 +3810,36 @@ function lastSurvivor(letters, coords) {
 }
 
 console.log(lastSurvivor("abc", [1, 1]));
+*/
+
+// #4
+/*
+A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern. The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where the pattern is: "add 3 to the previous term".
+In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+[ 0,  1,    3,      6,   ...]
+  0  0+1  0+1+2  0+1+2+3
+Your Task
+Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+Examples
+ 5  -->  [0,  1,  3,  6,  10,  15]
+-5  -->  [0, -1, -3, -6, -10, -15]
+ 7  -->  [0,  1,  3,  6,  10,  15,  21,  28]
+*/
+function sumOfN(n) {
+  let arr = [];
+
+  // Переменная для хранения текущей суммы
+  let sum = 0;
+
+  // Итерируюсь до Math.abs(n) для формирования последовательности
+  for (let i = 0; i <= Math.abs(n); i++) {
+    // Обновление суммы в соответствии с текущим индексом итерации и знаком "n"
+    sum += n < 0 ? -i : i;
+    arr.push(sum);
+  }
+
+  return arr;
+}
+
+console.log(sumOfN(3));
+console.log(sumOfN(-4));
