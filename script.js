0f70b2e6fa91a50eb2372ab29eb9314e12e0268e
DmitriyAngve <kirirch129@gmail.com>
2023-12-12 12:39:40 +0300

    productArray

--- script.js
+++ script.js
@@ -3913,6 +3913,8 @@ pyramid(2) => [ [1], [1, 1] ]
 pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
 Note: the subarrays should be filled with 1s
 */
+// FIRST
+/*
 function pyramid(n) {
   let result = [];
   for (let i = 0; i < n; i++) {
@@ -3924,8 +3926,67 @@ function pyramid(n) {
   }
   return result;
 }
-
+*/
+/*
+// SECOND
+function pyramid(n) {
+  return Array(n)
+    .fill()
+    .map((e, i) => Array(i + 1).fill(1));
+}
 console.log(pyramid(0));
 console.log(pyramid(1));
 console.log(pyramid(2));
 console.log(pyramid(3));
+*/
+
+// 12.12.2023
+
+// #1
+/*
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+Notes
+    Array/list size is at least 2 .
+    Array/list's numbers Will be only Positives
+    Repetition of numbers in the array/list could occur.
+Input >> Output Examples
+productArray ({12,20}) ==>  return {20,12}
+Explanation:
+    The first element in prod [] array 20 is the product of all array's elements except the first element
+    The second element 12 is the product of all array's elements except the second element .
+productArray ({1,5,2}) ==> return {10,2,5}
+Explanation:
+    The first element 10 is the product of all array's elements except the first element 1
+    The second element 2 is the product of all array's elements except the second element 5
+    The Third element 5 is the product of all array's elements except the Third element 2.
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+Explanation:
+    The first element 180 is the product of all array's elements except the first element 10
+    The second element 600 is the product of all array's elements except the second element 3
+    The Third element 360 is the product of all array's elements except the third element 5
+    The Fourth element 300 is the product of all array's elements except the fourth element 6
+    Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2
+*/
+function productArray(numbers) {
+  // FIRST
+  /*
+  const total = numbers.reduce((acc, num) => acc * num, 1);
+  console.log(total);
+  const result = numbers.map((num) => total / num);
+  return result;
+  */
+
+  const result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    let product = 1;
+    for (let j = 0; j < numbers.length; j++) {
+      if (i !== j) {
+        product *= numbers[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+}
+
+console.log(productArray([3, 27, 4, 2]));
