8239861a8b502e6f0e6745c55c30842bd4a3d819
DmitriyAngve <kirirch129@gmail.com>
2023-12-27 14:32:05 +0300

    automorphic

--- script.js
+++ script.js
@@ -4229,11 +4229,65 @@ console.log(jumpingNumber(23));
 console.log(jumpingNumber(79));
 console.log(jumpingNumber(8987));
 */
-const obj = {
-  prop1: "1",
-  prop2: "2",
-};
+// const obj = {
+//   prop1: "1",
+//   prop2: "2",
+// };
 
-console.log(obj.hasOwnProperty("prop1"));
-console.log("prop1" in obj);
-console.log(obj["prop1"]);
+// console.log(obj.hasOwnProperty("prop1"));
+// console.log("prop1" in obj);
+// console.log(obj["prop1"]);
+/*
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const squares = nums.map(function (num) {
+  return num * num;
+});
+
+console.log(squares);
+console.log(nums);
+*/
+
+// 27.12.2023
+// #1
+/*
+A number is called Automorphic number if and only if its square ends in the same digits as the number itself.
+Task
+Given a number determine if it Automorphic or not .
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+    The number passed to the function is positive
+    Input >> Output Examples
+autoMorphic (25) -->> return "Automorphic" 
+Explanation:
+    25 squared is 625 , Ends with the same number's digits which are 25 .
+    autoMorphic (13) -->> return "Not!!"
+    Explanation:
+    13 squared is 169 , Not ending with the same number's digits which are 69 .
+    autoMorphic (76) -->> return "Automorphic"
+    Explanation:
+    76 squared is 5776 , Ends with the same number's digits which are 76 .
+    autoMorphic (225) -->> return "Not!!"
+    Explanation:
+    225 squared is 50625 , Not ending with the same number's digits which are 225 .
+    autoMorphic (625) -->> return "Automorphic"
+    Explanation:
+    625 squared is 390625 , Ends with the same number's digits which are 625 .
+    autoMorphic (1) -->> return "Automorphic"
+    Explanation:
+    1 squared is 1 , Ends with the same number's digits which are 1 .
+    autoMorphic (6) -->> return "Automorphic"
+    Explanation:
+    6 squared is 36 , Ends with the same number's digits which are 6 
+*/
+function automorphic(n) {
+  const arr = n.toString().split("").map(Number);
+  let square = n * n;
+  let result = square.toString().split("").map(Number).slice(-arr.length);
+  return arr.join("") === result.join("") ? "Automorphic" : "Not!!";
+}
+
+console.log(automorphic(1));
+console.log(automorphic(225));
+console.log(automorphic(625));
