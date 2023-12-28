e59e15cf9d735249bf78acba13435d6232f66fab
DmitriyAngve <kirirch129@gmail.com>
2023-12-28 18:24:39 +0300

    menFromBoys

--- script.js
+++ script.js
@@ -4573,6 +4573,7 @@ The total number of bits will always be a multiple of 8.
 The data is given in an array as such:
 [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
 */
+/*
 function dataReverse(data) {
   const revData = [];
   for (let i = data.length - 8; i >= 0; i -= 8) {
@@ -4589,3 +4590,49 @@ const inputData = [
 ];
 const reversedOutput = dataReverse(inputData);
 console.log(reversedOutput);
+*/
+
+// #3
+/*
+Now that the competition gets tough it will Sort out the men from the boys .
+Men are the Even numbers and Boys are the odd !alt !alt
+Task
+Given an array/list [] of n integers , Separate The even numbers from the odds , or Separate the men from the boys !alt !alt
+Notes
+    Return an array/list where Even numbers come first then odds
+    Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+    Array/list size is at least 4 .
+    Array/list numbers could be a mixture of positives , negatives .
+    Have no fear , It is guaranteed that no Zeroes will exists . !alt
+    Repetition of numbers in the array/list could occur , So (duplications are not included when separating).
+    Input >> Output Examples:
+menFromBoys ({7, 3 , 14 , 17}) ==> return ({14, 17, 7, 3}) 
+Explanation:
+Since , { 14 } is the even number here , So it came first , then the odds in descending order {17 , 7 , 3} .
+menFromBoys ({-94, -99 , -100 , -99 , -96 , -99 }) ==> return ({-100 , -96 , -94 , -99})
+Explanation:
+    Since , { -100, -96 , -94 } is the even numbers here , So it came first in *ascending order *, then the odds in descending order { -99 }
+    Since , (Duplications are not included when separating) , then you can see only one (-99) was appeared in the final array/list .
+menFromBoys ({49 , 818 , -282 , 900 , 928 , 281 , -282 , -1 }) ==> return ({-282 , 818 , 900 , 928 , 281 , 49 , -1})
+Explanation:
+    Since , {-282 , 818 , 900 , 928 } is the even numbers here , So it came first in ascending order , then the odds in descending order { 281 , 49 , -1 }
+    Since , (Duplications are not included when separating) , then you can see only one (-282) was appeared in the final array/list .
+*/
+function menFromBoys(arr) {
+  let mans = [];
+  let boys = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      mans.push(arr[i]);
+    } else {
+      boys.push(arr[i]);
+    }
+  }
+  const result = new Set(
+    mans.sort((a, b) => a - b).concat(boys.sort((a, b) => b - a))
+  );
+
+  return [...result];
+}
+
+console.log(menFromBoys([82, 91, 72, 76, 76, 100, 85]));
