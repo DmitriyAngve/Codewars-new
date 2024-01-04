e00ff28109db0b2e6788ac8ea5c26570bd93859c
DmitriyAngve <kirirch129@gmail.com>
2024-01-04 17:04:41 +0300

    duplicateCount

--- script.js
+++ script.js
@@ -5185,6 +5185,7 @@ cachedFunction('foo', 'bar'); // complex function should be executed
 cachedFunction('foo', 'bar'); // complex function should not be invoked again, instead the cached result should be returned
 cachedFunction('foo', 'baz'); // should be executed, because the method wasn't invoked before with these arguments
 */
+/*
 function cache(func) {
   const cacheMap = new Map();
 
@@ -5200,3 +5201,38 @@ function cache(func) {
     return result;
   };
 }
+*/
+
+// #18
+/*
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+*/
+function duplicateCount(text) {
+  let ht = {};
+  let count = 0;
+
+  for (char of text.toLowerCase()) {
+    ht[char] = (ht[char] || 0) + 1;
+
+    if (ht[char] === 2) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(duplicateCount(""));
+console.log(duplicateCount("abcde"));
+console.log(duplicateCount("aabBcde"));
+console.log(duplicateCount("Indivisibility"));
+console.log(duplicateCount("Indivisibilities"));
