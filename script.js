9c78898c584080ab6ffcacba00d9098835470362
DmitriyAngve <kirirch129@gmail.com>
2024-01-04 11:16:56 +0300

    basicOp

--- script.js
+++ script.js
@@ -4660,6 +4660,7 @@ console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
 // 04.01.2023
 
 // ASTON
+// #1
 /*
 Very simple, given an integer or a floating-point number, find its opposite.
 Examples:
@@ -4667,6 +4668,7 @@ Examples:
 14: -14
 -34: 34
 */
+/*
 const opposite = (number) => {
   if (typeof number === "number") {
     return -number;
@@ -4678,3 +4680,36 @@ const opposite = (number) => {
 console.log(opposite(1));
 console.log(opposite(14));
 console.log(opposite(4.25));
+*/
+
+// #2
+/*
+Your task is to create a function that does four basic mathematical operations.
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+
+const basicOp = (operation, value1, value2) => {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+    default:
+      return NaN;
+  }
+};
+
+console.log(basicOp("+", 4, 7));
+console.log(basicOp("-", 15, 18));
+console.log(basicOp("*", 5, 5));
+console.log(basicOp("/", 49, 7));
