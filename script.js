10bffacd7a910253d2f37a80a61ac6233f978dde
DmitriyAngve <kirirch129@gmail.com>
2024-05-14 13:59:12 +0000

    createDict

--- script.js
+++ script.js
@@ -6979,7 +6979,7 @@ Examples:
 [1, 1, 1]  =>  [1]
 []  =>  []
 */
-
+/*
 function twoHighest(arr) {
   if (!arr.length || arr.length === 1) return arr;
   const res = [...new Set(arr)].sort((a, b) => b - a);
@@ -6989,3 +6989,33 @@ function twoHighest(arr) {
 console.log(twoHighest([4, 10, 10, 9]));
 console.log(twoHighest([15]));
 console.log(twoHighest([]));
+*/
+
+// #8
+/*
+There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
+Example 1:
+keys = ['a', 'b', 'c', 'd']
+values = [1, 2, 3]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
+Example 2:
+keys = ['a', 'b', 'c']
+values = [1, 2, 3, 4]
+createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
+*/
+
+function createDict(keys, values) {
+  let ht = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (i < values.length) {
+      ht[keys[i]] = values[i];
+    } else {
+      ht[keys[i]] = null;
+    }
+  }
+  return ht;
+}
+
+console.log(createDict(["a", "b", "c"], [1, 2, 3]));
+console.log(createDict(["a", "b", "c"], [1, 2, 3, 4]));
+console.log(createDict(["a", "b", "c", "d"], [1, 2, 3]));
