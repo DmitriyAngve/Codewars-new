1944e893b620b65f49e6b651bb710c049ad441cf
DmitriyAngve <kirirch129@gmail.com>
2024-06-05 15:45:03 +0300

    getStrings

--- script.js
+++ script.js
@@ -7296,7 +7296,7 @@ Given a sequence of integers, return the sum of all the integers that have an ev
 Indices in sequence start from 0.
 If the sequence is empty, you should return 0.
 */
-
+/*
 function evenLast(numbers) {
   if (!numbers.length) return 0;
 
@@ -7311,3 +7311,45 @@ function evenLast(numbers) {
 
 console.log(evenLast([2, 3, 4, 5]));
 console.log(evenLast([]));
+*/
+
+// #6
+/*
+You receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using asterisks (*).
+
+For example:
+
+"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+
+As you can see, the letter c is shown only once, but with 2 asterisks.
+
+The return string should include only the letters (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (,) as seen in the example above.
+
+Note that the return string must list the letters in order of their first appearance in the original string.
+
+More examples:
+
+"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+*/
+function getStrings(city) {
+  const str = city.replace(/\s/g, "").toLowerCase().split("");
+  let ht = {};
+
+  for (const el of str) {
+    ht[el] = (ht[el] || "") + "*";
+  }
+
+  let result = "";
+  for (const key in ht) {
+    if (result) {
+      result += ",";
+    }
+    result += `${key}:${ht[key]}`;
+  }
+
+  return result;
+}
+
+console.log(getStrings("Chicago"));
+console.log(getStrings("Las Vegas"));
