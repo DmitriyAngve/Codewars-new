d9f8d3e946513494ca40d18c80b829a2d419a7d4
DmitriyAngve <kirirch129@gmail.com>
2024-06-07 13:18:16 +0300

    alphabetWar

--- script.js
+++ script.js
@@ -7449,7 +7449,7 @@ Output:
 => We must remove every memories of Sarah Connor because she's marked, but as a side-effect we must also remove all the memories about Marilyn Monroe that comes right after her! Note that we can't destroy the memories of Manson Marilyn or Monroe Mary, so be careful!
 Fundamentals
 */
-
+/*
 function select(memory) {
   let names = memory.split(", ");
   console.log(names);
@@ -7478,3 +7478,57 @@ console.log(
     "Albert Einstein, !Sarah Connor, Marilyn Monroe, Abraham Lincoln, Sarah Connor, Sean Connery, Marilyn Monroe, Bjarne Stroustrup, Manson Marilyn, Monroe Mary"
   )
 );
+*/
+
+// 07.06.2024
+
+// #1
+/*
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+The left side letters and their power:
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims.
+Example
+alphabetWar("z");        //=> Right side wins!
+alphabetWar("zdqmwpbs"); //=> Let's fight again!
+alphabetWar("zzzzs");    //=> Right side wins!
+alphabetWar("wwwwwwz");  //=> Left s
+*/
+
+function alphabetWar(fight) {
+  const leftSide = { w: 4, p: 3, b: 2, s: 1 };
+  const rightSide = { m: 4, q: 3, d: 2, z: 1 };
+
+  let rightCount = 0;
+  let leftCount = 0;
+
+  for (const letter of fight) {
+    if (leftSide[letter]) {
+      leftCount += leftSide[letter];
+    } else if (rightSide[letter]) {
+      rightCount += rightSide[letter];
+    }
+  }
+
+  if (leftCount > rightCount) {
+    return "Left side wins!";
+  } else if (rightCount > leftCount) {
+    return "Right side wins!";
+  } else {
+    return "Let's fight again!";
+  }
+}
+
+console.log(alphabetWar("zdqmwpbs"));
+console.log(alphabetWar("zzzzs"));
