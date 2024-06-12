f1b68ccef324c5335d1db38409359b83783e9800
DmitriyAngve <kirirch129@gmail.com>
2024-06-12 16:26:52 +0300

    adviseBefore

--- script.js
+++ script.js
@@ -8174,7 +8174,7 @@ The format should be "KEY = VALUE". Each key/value pair should be separated by a
 Example:
 solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
 */
-
+/*
 function solution(pairs) {
   const result = Object.entries(pairs).map(
     ([key, value]) => `${key} = ${value}`
@@ -8185,3 +8185,30 @@ function solution(pairs) {
 console.log(solution({ "a": 1, "b": 2 }));
 console.log(solution({ "a": 1, "b": 2, "c": 3 }));
 console.log(solution({}));
+*/
+
+// 12.06.2024
+
+// #1
+/*
+Aspect-oriented programming (AOP) is programming that adds additional behavior (advice) to existing functionality without actually modifying that functionality.
+
+Create a method called adviseBefore. This method will take two arguments, a function (target) and the advising function (advice).
+
+adviseBefore should return a function that, when executed, will first execute the advising function and then the original method with the following conditions:
+
+    The arguments passed to the function that adviseBefore returns should be passed to the advising function.
+    If the advising function returns an array, the array should replace the arguments passed to the original method.
+    If the advising function does not return an array, the original arguments should be passed to the original method.
+    The return value of the original method should be returned.
+*/
+
+function adviseBefore(target, advice) {
+  return function (...args) {
+    const adviseArgs = advice(...args);
+    const finalArgs = Array.isArray(adviseArgs) ? adviseArgs : args;
+    return target(...finalArgs);
+  };
+}
+
+console.log(adviseBefore());
