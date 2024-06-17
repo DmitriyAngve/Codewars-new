12523648883a142d17cd8d913799fe3f8cba1e2a
DmitriyAngve <kirirch129@gmail.com>
2024-06-17 19:29:45 +0300

    express

--- script.js
+++ script.js
@@ -8359,7 +8359,7 @@ Here is a list of functions, we need:
     Math.ceil()
     Math.floor()
 */
-
+/*
 Math.round = function (number) {
   if (number % 1 >= 0.5) {
     return Math.floor(number) + 1;
@@ -8379,3 +8379,50 @@ Math.ceil = function (number) {
 Math.floor = function (number) {
   return number - (number % 1);
 };
+*/
+
+// #4
+/*
+Express is a minimalist web framework for Node.js.
+
+To get a server up and running there are a few steps you have to follow.
+
+    Install express npm install express --save
+    Require express on a file var express = require('express');
+    Get the app from express by invoking it var app = express();
+    Start the server with the app's listen function
+
+The listen function will start a server that you are going to be able to use through the browser.
+
+It has different signature methods, the one you are going to use in this kata is (port, host, callback)
+Your Task
+
+Inside the solution function you have to require express and start a server.
+
+The PORT and HOST are going to be pre-loaded in the process.env object (in a node application process.env stores the environment configuration).
+
+process.env = {
+  PORT: 3000,
+  HOST: '0.0.0.0',
+};
+*/
+const solution = () => {
+  var express = require("express");
+  var app = express();
+  // app.use(express.json());
+
+  // Start
+  const port = process.env.PORT;
+  const host = process.env.HOST;
+
+  app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+  });
+};
+
+process.env = {
+  PORT: 3000,
+  HOST: "0.0.0.0",
+};
+
+solution();
