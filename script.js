0548c5bc8cf569e33dc110e777a041cb0529b35c
DmitriyAngve <kirirch129@gmail.com>
2024-06-20 13:08:05 +0300

    padIt

--- script.js
+++ script.js
@@ -8406,10 +8406,11 @@ process.env = {
   HOST: '0.0.0.0',
 };
 */
+/*
 const solution = () => {
   var express = require("express");
   var app = express();
-  // app.use(express.json());
+
 
   // Start
   const port = process.env.PORT;
@@ -8420,9 +8421,38 @@ const solution = () => {
   });
 };
 
-process.env = {
-  PORT: 3000,
-  HOST: "0.0.0.0",
-};
+*/
 
-solution();
+// 20.06.2024
+
+// #1
+/*
+Complete function padIt, which accepts 2 parameters:
+
+    str: a string representing the string to pad. We will pad it with "*" symbols on its left side and on its right side.
+    n: a number indicating how many times to pad the string.
+
+Behaviour
+
+You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+Finally, return the padded string.
+*/
+function padIt(str, n) {
+  let i = 0;
+  while (i < n) {
+    if (i % 2 === 0) {
+      str = "*" + str;
+    } else {
+      str = str + "*";
+    }
+    i++;
+  }
+  return str;
+}
+
+console.log(padIt("a", 1));
+console.log(padIt("a", 2));
+console.log(padIt("a", 3));
+console.log(padIt("a", 4));
+console.log(padIt("a", 5));
