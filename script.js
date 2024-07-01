d62f28cf4414987c5937abbe2f9a23f1071fe48d
DmitriyAngve <kirirch129@gmail.com>
2024-07-01 13:59:42 +0300

    getNumberLength

--- script.js
+++ script.js
@@ -8639,6 +8639,7 @@ instructions = [1,2,4,3,5]
 // Should return
 "5 4 3 2 1 liftoff!"
 */
+/*
 function liftoff(instructions) {
   let sorted = instructions.sort((a, b) => b - a);
   sorted.push("liftoff!");
@@ -8648,3 +8649,20 @@ function liftoff(instructions) {
 
 console.log(liftoff([8, 1, 10, 2, 7, 9, 6, 3, 4, 5]));
 console.log(liftoff([1, 2, 4, 3, 5]));
+*/
+
+// #8
+/*
+Exponential/scientific notation is used to write very large or small numbers in JavaScript. It uses positive and negative exponents to write multiples and submultiples of 10 and simplifies numbers by getting rid of zeros. If a number greater than 21 digits, in JavaScript, it is displayed in exponential notation and if you try to get the length of it by converting to the string you would get wrong number of length because of the specific way of its display.
+Write a function which returns the number of digits in any number with more than 21 digits. If the function is passed a string or a number with 21 or fewer digits it should return "wrong input".
+For example: If input number is 9000000000000000000000, then it should return number of digits, in this case 22. If numbers are 222222222 or "5263" which number of digits are less than 21 or "asdf///" which is not a number then it should return string 'wrong input'.
+*/
+
+function getNumberLength(n) {
+  if (typeof n === "string" || !n.toString().includes("e+"))
+    return "wrong input";
+  return parseInt(n.toString().substring(n.toString().indexOf("e+") + 2)) + 1;
+}
+console.log(getNumberLength(7005623021525000000000000000));
+console.log(getNumberLength(5236200000000000));
+console.log(getNumberLength(9000000000000000000000));
