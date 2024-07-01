dbb90fa98b6c42b3daaa4a7721e5fc4e15086363
DmitriyAngve <kirirch129@gmail.com>
2024-07-01 14:30:59 +0300

    validSpacing

--- script.js
+++ script.js
@@ -8707,6 +8707,7 @@ Examples
 "App7Test"        -->  "app7_test"
 1                 -->  "1"
 */
+/*
 function toUnderscore(string) {
   if (typeof string === "number") return string.toString();
 
@@ -8721,3 +8722,33 @@ console.log(toUnderscore(1));
 console.log(toUnderscore("TestController"));
 console.log(toUnderscore("MoviesAndBooks"));
 console.log(toUnderscore("App7Test"));
+*/
+
+// #11
+/*
+Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either true or false (or the corresponding value in each language).
+
+For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Words can be any consecutive sequence of non space characters. Below are some examples of what the function should return:
+
+* 'Hello world'   => true
+* ' Hello world'  => false
+* 'Hello world  ' => false
+* 'Hello  world'  => false
+* 'Hello'         => true
+
+Even though there are no spaces, it is still valid because none are needed:
+* 'Helloworld'    => true
+* 'Helloworld '   => false
+* ' '             => false
+* ''              => true
+*/
+
+function validSpacing(s) {
+  return s.trim() === s && !s.includes("  ");
+}
+
+console.log(validSpacing("Hello world"));
+console.log(validSpacing(" Hello world"));
+console.log(validSpacing("Hello world  "));
+console.log(validSpacing("  "));
+console.log(validSpacing(""));
