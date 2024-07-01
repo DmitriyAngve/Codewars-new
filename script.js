add9e676788549af8d41d4d70a48bdc279d15be6
DmitriyAngve <kirirch129@gmail.com>
2024-07-01 12:55:41 +0300

    isSparse

--- script.js
+++ script.js
@@ -8533,6 +8533,7 @@ SafeInteger(9007199254740990) //true
 SafeInteger(-90) //true
 SafeInteger(9007199254740992) //false
 */
+/*
 function SafeInteger(n) {
   return Number.isSafeInteger(n);
 }
@@ -8540,3 +8541,22 @@ function SafeInteger(n) {
 console.log(SafeInteger(9007199254740992));
 console.log(SafeInteger(-90));
 console.log(SafeInteger(9007199254740992));
+*/
+
+// #4
+/*
+JavaScript has sparse arrays, in which not all of its elements are necessarily set. As a result, the length of a javascript array may be greater than the number of its elements. It would be useful to have an array method that checks whether or not an array is sparse.
+
+The challenge here is to create such a method. Please remember that an element in an array can be set to anything including "null" and "undefined". Such elements are not considered sparse in JavaScript. One of the ways to make sparse arrays is for example by deleting some elements in a regular array, like in "delete array[i]", where i < array.length. Also remember that an empty array is not a sparse array by definition.
+*/
+
+Array.prototype.isSparse = function () {
+  for (let i = 0; i < this.length; i++) {
+    if (!(i in this)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log([1, 2, 3].isSparse());
