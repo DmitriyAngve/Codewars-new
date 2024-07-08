287f06ffc5514f3507f848b5653ef9cf43526bc2
DmitriyAngve <kirirch129@gmail.com>
2024-07-08 09:26:17 +0300

    bingo

--- script.js
+++ script.js
@@ -9123,6 +9123,7 @@ Notes:
     If null value given then return -1
 */
 //return price without vat
+/*
 function excludingVatPrice(price) {
   if (price === null) return -1;
   const vat = 1.15;
@@ -9132,3 +9133,27 @@ function excludingVatPrice(price) {
 
 console.log(excludingVatPrice(230));
 console.log(excludingVatPrice(123));
+*/
+
+// #3
+/*
+For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+*/
+
+function bingo(a) {
+  const bingo = new Set(["B", "I", "N", "G", "O"]);
+  const letters = a.map((num) => String.fromCharCode(num + 64));
+  const unique = new Set(letters);
+
+  for (let letter of bingo) {
+    if (!unique.has(letter)) {
+      return "LOSE";
+    }
+  }
+  return "WIN";
+}
+
+console.log(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10]));
