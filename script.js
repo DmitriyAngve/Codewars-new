9e6d2cf658a4dbdcb2e37771535d7aab356599db
DmitriyAngve <kirirch129@gmail.com>
2024-07-15 13:54:45 +0300

    sequence

--- script.js
+++ script.js
@@ -9171,14 +9171,13 @@ Jack really likes his number five: the trick here is that you have to multiply e
   0 -->     0  (  0 * 5¹)
  -3 -->   -15  ( -3 * 5¹)
 */
-
+/*
 function multiply(number) {
   let len = number.toString().length;
   if (number < 0) {
     len = len - 1;
   }
 
-  console.log(length);
   return number * Math.pow(5, len);
 }
 
@@ -9187,3 +9186,55 @@ function multiply(number) {
 // console.log(multiply(200));
 // console.log(multiply(0));
 console.log(multiply(-3));
+*/
+
+// #2
+/*
+Implement function sequence, which returns new n-size Array filled according to pattern.
+
+pattern may be:
+
+    a function that takes two: (element, index), one: (element) or any arguments (similar to map function), then filled running this function, in other words: function describes sequence,
+    number, string or any other object, then filled by copying, this object n-times.
+
+Examples:
+
+sequence(3, 4); // [4, 4, 4]
+sequence(5, []); // [[], [], [], [], []]
+sequence(2, "s"); // ["s", "s"]
+sequence(5, (x, idx) => idx%2) // [0, 1, 0, 1, 0];
+sequence(10, (x, idx) => idx+1) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Note: Sequences are great to work with functional methods like map, reduce, forEach, every or any. For example:
+
+// sum of numbers 1-10
+let sum = sequence(10, (x, idx) => idx+1).reduce((sum, num) => sum + num);
+
+Be careful with long sequences. They are just arrays, every element is created when function is called.
+
+For lazy sequences (elements created when needed) use Iterator.
+*/
+
+function sequence(n, pattern) {
+  // const res = new Array(n);
+  // if (typeof pattern === "function") {
+  //   for (let i = 0; i < n; i++) {
+  //     res[i] = pattern(res[i], i);
+  //   }
+  // } else {
+  //   res.fill(pattern);
+  // }
+  // return res;
+
+  return Array.from(
+    { length: n },
+    typeof pattern === "function" ? pattern : () => pattern
+  );
+
+  // () => pattern - это стрелочная функция, которая всегда возвращает pattern
+}
+
+console.log(sequence(3, 4));
+console.log(sequence(3, "s"));
+console.log(sequence(3, []));
+console.log(sequence(3, (x, idx) => idx % 2));
