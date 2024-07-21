d90ca5953d28520a1f157906208ef86490b2c01e
DmitriyAngve <kirirch129@gmail.com>
2024-07-21 17:16:12 +0300

    madShout

--- script.js
+++ script.js
@@ -9892,7 +9892,7 @@ Rules
 
 6. If you find yourself still in the maze after using all the moves, you should return Lost.
 */
-
+/*
 function mazeRunner(maze, directions) {
   // Определение размеров лабиринта
   const n = maze.length;
@@ -9992,3 +9992,55 @@ console.log(
     ["N", "N", "N", "W", "W"]
   )
 );
+*/
+
+// #10
+/*
+You are walking on the sidwalk, you notice your friend is also walking on the sidewalk. You need to give a shout to your friend in order to grab their attention.
+Kata
+
+Write the function madShout that takes a string input sidewalk to calculate the shout string and return it.
+
+The shout phrase is "Oi F!" but depending on the distance between your friend, the word expands.
+Example for input 'sidewalk'
+
+'---------Y-----F------------'
+
+Y = You
+F = Friend
+
+'Shout' phrase:
+
+Since in the example, the difference between the friends is 6 (F's index is 15 & that of Y's is 9), the shout phrase becomes 'Oiii F!'
+Consider the following differences and its output:
+
+Difference is 4 => 'Oii F!'
+2 => 'Oi F!'
+9 => 'Oiiiii F!'
+0 => 'Oi F!'
+1 => 'Oi F!'
+20 => 'Oiiiiiiiiii F!'
+
+Some guides for coding
+
+    Y will always be on the left and F will be on the right.
+    The number of 'i's in the output must be >= 1
+    All the inputs are valid.
+*/
+
+function madShout(sidewalk) {
+  let yIndex = sidewalk.indexOf("Y");
+  let fIndex = sidewalk.indexOf("F");
+
+  if (yIndex === -1 || fIndex === -1) {
+    return "Error: Missing Y or F in the input.";
+  }
+
+  let distance = fIndex - yIndex;
+  let shout = "Oi" + "i".repeat(Math.max(1, Math.floor(distance / 2))) + " F!";
+
+  return shout;
+}
+
+// console.log(madShout("-----------------Y--------F--------------"));
+console.log(madShout("------------Y-F------------------"));
