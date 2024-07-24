a6281e843870f1bf2ab6135b8fba75bbf8d3b0e6
DmitriyAngve <kirirch129@gmail.com>
2024-07-24 15:45:15 +0300

    numberJoy

--- script.js
+++ script.js
@@ -10344,7 +10344,7 @@ var group = {
 
 splitTheBill(group) // returns {A: 5, B: 0, C: -5}
 */
-
+/*
 function splitTheBill(x) {
   let total = 0;
   for (const amount of Object.values(x)) {
@@ -10364,3 +10364,35 @@ function splitTheBill(x) {
 }
 
 console.log(splitTheBill({ A: 20, B: 15, C: 10 }));
+*/
+
+// 24.07.2024
+// #1
+/*
+Positive integers that are divisible exactly by the sum of their digits are called Harshad numbers. The first few Harshad numbers are: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 18, ...
+
+We are interested in Harshad numbers where the product of its digit sum s and s with the digits reversed, gives the original number n. For example consider number 1729:
+
+    its digit sum, s = 1 + 7 + 2 + 9 = 19
+    reversing s = 91
+    and 19 * 91 = 1729 --> the number that we started with.
+
+Complete the function which tests if a positive integer n is Harshad number, and returns True if the product of its digit sum and its digit sum reversed equals n; otherwise return False.
+*/
+
+function numberJoy(n) {
+  if (n <= 9) return true;
+
+  let arr = n.toString().split("").map(Number);
+
+  arr = arr.reduce((acc, curr) => acc + curr, 0);
+
+  let revArr = Number(arr.toString().split("").reverse().join(""));
+
+  return arr * revArr === n;
+}
+
+console.log(numberJoy(1997));
+console.log(numberJoy(1));
+console.log(numberJoy(1729));
+console.log(numberJoy(1458));
