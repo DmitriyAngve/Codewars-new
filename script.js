de4106165f48f4b5507bddf4d12160e8593542f9
DmitriyAngve <kirirch129@gmail.com>
2024-08-08 16:37:16 +0300

    sumDynamic

--- script.js
+++ script.js
@@ -10838,7 +10838,7 @@ Code Examples
     prefill("xyz", 1)
       --> throws TypeError with message "xyz is invalid"
 */
-
+/*
 function prefill(n, v) {
   if (
     !Number.isInteger(Number(n)) ||
@@ -10853,3 +10853,56 @@ function prefill(n, v) {
 
 console.log(prefill(3, 1));
 console.log(prefill(2, "abc"));
+*/
+
+// 08.08.2024
+// #1
+/*
+How many ways can you make the sum of a number?
+
+From wikipedia: https://en.wikipedia.org/wiki/Partition_(number_theory)
+
+    In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers. Two sums that differ only in the order of their summands are considered the same partition. If order matters, the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
+
+4
+3 + 1
+2 + 2
+2 + 1 + 1
+1 + 1 + 1 + 1
+
+Examples
+Basic
+
+sum(1) // 1
+sum(2) // 2  -> 1+1 , 2
+sum(3) // 3 -> 1+1+1, 1+2, 3
+sum(4) // 5 -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
+sum(5) // 7 -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
+
+sum(10) // 42
+
+Explosive
+
+sum(50) // 204226
+sum(80) // 15796476
+sum(100) // 190569292
+*/
+
+function sum(num) {
+  // Создаю массив для хранения количества разбиений для каждого числа до num включительно
+  const partitions = Array(num + 1).fill(0);
+  partitions[0] = 1; // Базовый случай
+  // Проходим по каждому числу, которое может быть использовано в разбиении
+  for (let i = 1; i <= num; i++) {
+    // Обновляем массив partititons для каждого числа от i до num
+    for (let j = i; j <= num; j++) {
+      partitions[j] += partitions[j - i];
+    }
+  }
+  return partitions[num];
+}
+
+console.log(sum(1));
+console.log(sum(4));
+console.log(sum(5));
+console.log(sum(80));
