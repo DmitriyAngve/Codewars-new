8274d6c9ebb06e1eb2086de0aa39df4e9168b4c3
DmitriyAngve <kirirch129@gmail.com>
2024-08-12 14:10:48 +0300

    unscrambleEggs

--- script.js
+++ script.js
@@ -10984,7 +10984,7 @@ If it goes past 'z', start again at 'a'.
 
 Input will be a string with length > 0.
 */
-
+/*
 function moveTen(s) {
   let res = "";
 
@@ -11003,3 +11003,22 @@ function moveTen(s) {
 
 console.log(moveTen("testcase"));
 console.log(moveTen("codewars"));
+*/
+
+// #3
+/*
+Unscramble the eggs.
+
+The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+Example
+
+unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//             "B---eg---in---n---er---"
+
+Kata is supposed to be for beginners to practice regular expressions, so commenting would be appreciated.
+*/
+function unscrambleEggs(word) {
+  return word.replace(/egg/gi, "");
+}
+
+console.log(unscrambleEggs("Beggegeggineggneggeregg"));
