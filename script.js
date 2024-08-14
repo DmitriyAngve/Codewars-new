5444875762079c04ecdf5fc0f576a5c66ad861dc
DmitriyAngve <kirirch129@gmail.com>
2024-08-14 14:14:15 +0300

    nextHappyYear

--- script.js
+++ script.js
@@ -11280,7 +11280,7 @@ Example (s, sep --> Output)
 "19999999"   , ":" --> "1:7:7:7:7:7:7:7"
 "^^^**$"     , "x" --> "3x3x3x2x2x1"
 */
-
+/*
 function freqSeq(str, sep) {
   let ht = {};
   let res = [];
@@ -11297,3 +11297,52 @@ function freqSeq(str, sep) {
 
 console.log(freqSeq("hello world", "-"));
 console.log(freqSeq("^^^**$", "x"));
+*/
+
+// 14.08.2024
+// #1
+/*
+You're saying good-bye your best friend , See you next happy year .
+Happy Year is the year with only distinct digits , (e.g) 2018
+Task
+Given a year, Find The next happy year or The closest year You'll see your best friend !alt !alt
+Notes
+    Year Of Course always Positive .
+    Have no fear , It is guaranteed that the answer exists .
+    It's not necessary that the year passed to the function is Happy one .
+    Input Year with in range (1000  ≤  y  ≤  9000)
+Input >> Output Examples:
+nextHappyYear (7712) ==> return (7801)
+Explanation:
+As the Next closest year with only distinct digits is 7801 .
+nextHappyYear (8989) ==> return (9012)
+Explanation:
+As the Next closest year with only distinct digits is 9012 .
+nextHappyYear (1001) ==> return (1023)
+Explanation:
+As the Next closest year with only distinct digits is 1023 .
+*/
+
+function nextHappyYear(year) {
+  year++;
+
+  while (true) {
+    let arr = year.toString().split("");
+    let setSize = new Set(arr).size;
+    if (arr.length === setSize) {
+      return year;
+    }
+    year++;
+  }
+}
+
+console.log(nextHappyYear(1001));
+console.log(nextHappyYear(2334));
+console.log(nextHappyYear(8999));
+
+// while (i < arr.length) {
+//   if (arr[0] % arr[i] !== 0) {
+//     return false;
+//   }
+//   i++;
+// }
