a78998b148539e2cfc48f4acc8a6bfb03f4dad89
DmitriyAngve <kirirch129@gmail.com>
2024-08-14 14:46:02 +0300

    beeramid

--- script.js
+++ script.js
@@ -11353,6 +11353,7 @@ For example(Input --> Output):
  1 --> [1]
 */
 
+/*
 function monkeyCount(n) {
   // let arr = [];
   // for (let i = 1; i <= n; i++) {
@@ -11365,3 +11366,49 @@ function monkeyCount(n) {
 }
 
 console.log(monkeyCount(5));
+*/
+
+// #3
+/*
+Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+
+A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+
+Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+
+    your referral bonus, and
+
+    the price of a beer can
+
+For example:
+
+beeramid(1500, 2); // should === 12
+beeramid(5000, 3); // should === 16
+*/
+
+// Returns number of complete beeramid levels
+var beeramid = function (bonus, price) {
+  if (bonus <= 0 || price <= 0) return 0;
+
+  let levels = 0;
+  let totalCost = 0;
+
+  while (true) {
+    levels++;
+
+    const cansNeeded = levels * levels;
+    const costForLevel = cansNeeded * price;
+
+    if (totalCost + costForLevel <= bonus) {
+      totalCost += costForLevel;
+    } else {
+      break;
+    }
+  }
+
+  return levels - 1;
+};
+
+console.log(beeramid(10, 2));
+console.log(beeramid(454, 4));
+console.log(beeramid(-1, 4));
