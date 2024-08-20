c653a32ec9344bfdeda55c74d1a01613477359f5
DmitriyAngve <kirirch129@gmail.com>
2024-08-20 17:55:54 +0300

    points

--- script.js
+++ script.js
@@ -11828,7 +11828,7 @@ Some cases:
 
 [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 */
-
+/*
 function multipleOfIndex(array) {
   let result = [];
   if (array[0] === 0) {
@@ -11849,4 +11849,53 @@ console.log(
     -56, -85, 72, -26, -14, 76, -27, 72, 35, -21, -67, 87, 0, 21, 59, 27, -92,
     68,
   ])
+);
+*/
+
+// 20.08.2024
+// #1
+/*
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Points are awarded for each match as follows:
+
+    if x > y: 3 points (win)
+    if x < y: 0 points (loss)
+    if x = y: 1 point (tie)
+
+We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+    our team always plays 10 matches in the championship
+    0 <= x <= 4
+    0 <= y <= 4
+*/
+
+function points(games) {
+  let count = 0;
+  for (let i = 0; i < games.length; i++) {
+    let x = games[i].split("").map(Number);
+    if (x[0] > x[2]) {
+      count += 3;
+    }
+    if (x[0] === x[2]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])
+);
+console.log(
+  points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"])
+);
+console.log(
+  points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"])
 );
