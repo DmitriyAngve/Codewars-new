037b97f1cd443c7e21eedde31a84175493489d46
DmitriyAngve <kirirch129@gmail.com>
2024-08-20 18:04:57 +0300

    countPositivesSumNegatives

--- script.js
+++ script.js
@@ -11875,7 +11875,7 @@ Notes:
     0 <= x <= 4
     0 <= y <= 4
 */
-
+/*
 function points(games) {
   let count = 0;
   for (let i = 0; i < games.length; i++) {
@@ -11899,3 +11899,45 @@ console.log(
 console.log(
   points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"])
 );
+*/
+
+// #2
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+Example
+
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+  if (!input || input.length === 0) return [];
+
+  let posCount = 0;
+  let negSum = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      posCount++;
+    } else if (input[i] < 0) {
+      negSum += input[i];
+    }
+  }
+
+  return [posCount, negSum];
+}
+
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
+console.log(
+  countPositivesSumNegatives([
+    0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
+  ])
+);
+console.log(countPositivesSumNegatives([0, 0]));
