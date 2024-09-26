8bad69a209fd4071d9739979b681907cad7ffd13
DmitriyAngve <kirirch129@gmail.com>
2024-09-26 10:50:00 +0300

    gooseFilter

--- script.js
+++ script.js
@@ -12354,7 +12354,7 @@ Examples
 [1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
 [0, 1, 2, 3, 4, 5]                ==>  []
 */
-
+/*
 function duplicates(arr) {
   let counts = new Map(); // используем Map для учета типов данных
   let result = [];
@@ -12374,3 +12374,50 @@ function duplicates(arr) {
 }
 
 console.log(duplicates([1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]));
+*/
+
+// 26.09.2024
+
+// #1
+/*
+Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+*/
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  let result = [];
+
+  for (let i = 0; i < birds.length; i++) {
+    if (!geese.includes(birds[i])) {
+      result.push(birds[i]);
+    }
+  }
+  return result;
+}
+
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ])
+);
