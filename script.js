c4294f0a2d488cb4201c04ebf6cce78139060d50
DmitriyAngve <kirirch129@gmail.com>
2024-10-02 16:57:09 +0300

    grabDoll

--- script.js
+++ script.js
@@ -13164,6 +13164,7 @@ Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
 Input: ["sheep", "sheep", "wolf"]
 Output: "Pls go away and stop eating my sheep"
 */
+/*
 function warnTheSheep(queue) {
   let wolfIndex = queue.indexOf("wolf");
   console.log(wolfIndex);
@@ -13189,3 +13190,45 @@ console.log(
   ])
 );
 console.log(warnTheSheep(["wolf"]));
+*/
+
+// 02.10.2024
+/*
+Coding in function grabDoll. function accept 1 parameter:dolls. it's a string array, a list of some dolls.
+
+You need traverse dolls by using for loop. If element is "Hello Kitty" or "Barbie doll", you should push it to a bag(bag is an array, I've defined in the function); if it's other strings, we should use continue skip it.
+
+When the bag has three element, bag is full. You should use break jump out the loop; If bag is not full, you should traverse dolls until the last element.
+
+Return the bag after for loop finished.
+
+You should use for, break and continue in your code. otherwise, your solution may not pass this kata.
+
+If you forgot how to push an element to array, please refer to lesson 4.
+*/
+
+function grabDoll(dolls) {
+  var bag = [];
+  let count = 0;
+
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+      count++;
+      bag.push(dolls[i]);
+      if (count < 3) continue;
+      if (count === 3) break;
+    }
+  }
+  return bag;
+}
+
+console.log(
+  grabDoll([
+    "Mickey Mouse",
+    "Barbie doll",
+    "Hello Kitty",
+    "Hello Kitty",
+    "Hello Kitty",
+    "Snow white",
+  ])
+);
