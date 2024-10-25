c490d3815cc29839580a54b8ef22b87d7094a87a
DmitriyAngve <kirirch129@gmail.com>
2024-10-25 13:32:17 +0300

    nbDig

--- script.js
+++ script.js
@@ -13442,7 +13442,7 @@ Notes:
 
     See Examples Test Cases for more examples.
 */
-
+/*
 function rank(st, we, n) {
   if (!st) return "No participants";
   let arr = st.split(",");
@@ -13478,3 +13478,43 @@ console.log(
     4
   )
 );
+*/
+
+// 25.10.2024
+// #1
+/*
+Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Implement the function taking n and d as parameters and returning this count.
+Examples:
+
+n = 10, d = 1 
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+The function, when given n = 25 and d = 1 as argument, should return 11 since
+the k*k that contain the digit 1 are:
+1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+So there are 11 digits 1 for the squares of numbers between 0 and 25.
+
+Note that 121 has twice the digit 1.
+*/
+
+function nbDig(n, d) {
+  let count = 0;
+  const dig = d.toString();
+
+  for (let i = 0; i <= n; i++) {
+    const squareStr = (i ** 2).toString();
+    // console.log(squareStr);
+    count += squareStr.split(dig).length - 1;
+  }
+  return count;
+}
+
+console.log(nbDig(5750, 0));
+console.log(nbDig(11011, 2));
