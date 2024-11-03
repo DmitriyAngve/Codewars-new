5c09bdc6cd8db52fbe63bddfa85c4489f85e9ca7
DmitriyAngve <kirirch129@gmail.com>
2024-11-03 18:16:43 +0300

    tailSwap

--- script.js
+++ script.js
@@ -13888,6 +13888,7 @@ Here is an example of your return
 
 [3.6, "udiwstagwo"]
 */
+/*
 function mean(lst) {
   let sum = 0;
   let count = 0;
@@ -13929,3 +13930,24 @@ console.log(
     "0",
   ])
 );
+*/
+
+// #4
+/*
+You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+Examples
+
+["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+*/
+
+function tailSwap(arr) {
+  let f = arr[0].split(":");
+  let s = arr[1].split(":");
+
+  return [`${f[0]}:${s[1]}`, `${s[0]}:${f[1]}`];
+}
+
+console.log(tailSwap(["abc:123", "cde:456"]));
