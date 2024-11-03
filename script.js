a0922dd7d9139694d06bbb91867f4c8e2861759b
DmitriyAngve <kirirch129@gmail.com>
2024-11-03 17:49:26 +0300

    dominator

--- script.js
+++ script.js
@@ -13803,6 +13803,7 @@ For instance:
 
 "a   **&  cZ"  =>  "10100000000000000000000001"
 */
+/*
 function change(string) {
   const alphabet = {
     a: 0,
@@ -13843,3 +13844,32 @@ function change(string) {
 }
 
 console.log(change("a **&  bZ"));
+*/
+
+// #2
+/*
+A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+*/
+function dominator(arr) {
+  const freq = {};
+  const hl = arr.length / 2;
+
+  for (const num of arr) {
+    freq[num] = (freq[num] || 0) + 1;
+
+    if (freq[num] > hl) {
+      return num;
+    }
+  }
+  console.log(freq);
+
+  return -1;
+}
+
+console.log(dominator([3, 4, 3, 2, 3, 1, 3, 3]));
+console.log(dominator([1, 2, 3, 4, 5]));
+console.log(dominator([1, 1, 1, 2, 2, 2, 2]));
+console.log(dominator([1, 1, 1, 2, 2, 2]));
