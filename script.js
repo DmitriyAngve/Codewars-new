742e31cb7db299196b44a552d361db5521ed06c7
DmitriyAngve <kirirch129@gmail.com>
2024-12-06 15:03:35 +0300

    questions

--- script.js
+++ script.js
@@ -14342,6 +14342,7 @@ describe("Sample tests", () => {
   //click "Submit" try more testcase...
 });
 */
+/*
 function testit(s) {
   return s.replace(/../g, (s) =>
     String.fromCharCode((s.charCodeAt(0) + s.charCodeAt(1)) / 2)
@@ -14356,3 +14357,56 @@ console.log(testit("ba"));
 
 console.log(testit("abab"));
 console.log(testit("aaaaaa"));
+*/
+
+// #3
+/*
+Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+For example:
+
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+After adding the property the result should be:
+
+var questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0,
+    usersAnswer: null
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+    corAnswer: 0,
+    usersAnswer: null
+}];
+
+The questions array is already defined for you and is not the same as the one in the example.
+*/
+
+questions.forEach(function (i) {
+  i.usersAnswer = null;
+});
+
+var questions = [
+  {
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0,
+  },
+  {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0,
+  },
+];
+
+console.log(propertyToObj(questions));
