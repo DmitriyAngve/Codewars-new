ce0493bf60cbe65216cda6ef4400862b844dfaf8
DmitriyAngve <kirirch129@gmail.com>
2024-12-13 15:04:24 +0300

    Calculator

--- script.js
+++ script.js
@@ -14982,6 +14982,7 @@ Examples
 1244   => 12
 952406 => 9
 */
+/*
 const prevMultOfThree = (n) => {
   if (n % 3 === 0) return n;
 
@@ -14996,3 +14997,31 @@ console.log(prevMultOfThree(1));
 console.log(prevMultOfThree(25));
 console.log(prevMultOfThree(36));
 console.log(prevMultOfThree(1244));
+*/
+
+// #5
+/*
+Let's build a calculator.
+
+This is very basic Javascript kata.
+
+The test expects you to provide a Calculator object with the following methods:
+
+Calculator.add()
+Calculator.subtract()
+Calculator.multiply()
+Calculator.divide()
+
+Each method expects two arguments, so for instance:
+
+Calculator.add(1,4) should return 5.
+
+Only integers are expected to be passed in as arguments, and the divide method should return 'false' when trying the divide by zero.
+*/
+
+var Calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => (b === 0 ? false : a / b),
+};
