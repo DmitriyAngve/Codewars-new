2b429dbabdd7c160f62b8e68f90278af4341ff57
DmitriyAngve <kirirch129@gmail.com>
2024-12-23 14:56:35 +0300

    persistence

--- script.js
+++ script.js
@@ -15384,6 +15384,7 @@ Examples
 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
 0, "one", "two"    -->  []
 */
+/*
 function alternate(n, f, s) {
   let arr = Array.from({ length: n }, (_, index) => f);
   for (let i = 0; i < arr.length; i++) {
@@ -15396,3 +15397,35 @@ function alternate(n, f, s) {
 
 console.log(alternate(5, true, false));
 // const arr = Array.from({ length: n }, (_, index) => index + 1);
+*/
+
+// #3
+/*
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2, there are 4 multiplications)
+4 --> 0 (because 4 is already a one-digit number, there is no multiplication)
+*/
+function persistence(num) {
+  let count = 0;
+  while (num >= 10) {
+    num = multiple(num);
+    count++;
+  }
+  return count;
+}
+
+function multiple(num) {
+  return num
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce((acc, curr) => acc * curr, 1);
+}
+
+console.log(persistence(39));
+console.log(persistence(999));
+console.log(persistence(4));
