aaca0ebc35c21f0d7aaf0aadb88a557931eda8b5
DmitriyAngve <kirirch129@gmail.com>
2024-12-24 15:23:52 +0300

    formatMoney

--- script.js
+++ script.js
@@ -15664,6 +15664,7 @@ C          100
 D          500
 M          1,000
 */
+/*
 function solution(num) {
   let res = "";
   // Словарь римских цифр, упорядоченных по убыванию
@@ -15694,3 +15695,40 @@ function solution(num) {
 
 console.log(solution(2008));
 console.log(solution(1990));
+*/
+
+// #2
+/*
+The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+39.99 becomes $39.99
+
+The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+Examples:
+
+3 needs to become $3.00
+
+3.1 needs to become $3.10
+*/
+function formatMoney(amount) {
+  let arr = String(amount).split(".");
+  let res = "";
+
+  if (arr.length === 2) {
+    if (arr[1].length === 1) {
+      res = `$${arr[0]}.${arr[1]}0`;
+    } else if (arr[1].length === 2) {
+      res = `$${arr[0]}.${arr[1]}`;
+    }
+  } else {
+    res = `$${arr[0]}.00`;
+  }
+
+  return res;
+}
+console.log(formatMoney(3.1));
+console.log(formatMoney(3));
+console.log(formatMoney(39.99));
+console.log(formatMoney(631.9));
+console.log(formatMoney(631.99));
