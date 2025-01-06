b81a882f4b6be8dc84d56c14eb3286fc510a9798
DmitriyAngve <kirirch129@gmail.com>
2025-01-06 14:14:28 +0300

    getMissingElement

--- script.js
+++ script.js
@@ -16490,6 +16490,7 @@ For example:
 solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 solution(null); // should return []
 */
+/*
 function solution(nums) {
   if (!Array.isArray(nums)) return [];
   if (nums.length === 0) return [];
@@ -16499,3 +16500,38 @@ function solution(nums) {
 
 console.log(solution([]));
 console.log(solution([20, 2, 10]));
+*/
+
+// #3
+/*
+Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+Examples:
+
+[0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
+[9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
+*/
+
+function getMissingElement(superImportantArray) {
+  if (!superImportantArray.length) return 0;
+  let arr = superImportantArray.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i] + 1;
+    }
+  }
+  if (arr[0] !== 0) {
+    return 0;
+  }
+  if (arr[arr.length - 1] !== 9) {
+    return 9;
+  }
+}
+
+console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]));
+console.log(getMissingElement([8, 5, 1, 3, 2, 9, 7, 6, 4]));
+console.log(getMissingElement([3, 2, 4, 5, 7, 0, 8, 6, 1]));
