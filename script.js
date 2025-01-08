a37d7da7e00b15850b84c5684fdf8744073a6ace
DmitriyAngve <kirirch129@gmail.com>
2025-01-08 14:35:17 +0300

    timeToMinutes

--- script.js
+++ script.js
@@ -16777,6 +16777,7 @@ blackAndWhite([5,12]) should return "It's a white array"
 
 Using string template and ternary operator can make your work easier.
 */
+/*
 function blackAndWhite(arr) {
   if (!Array.isArray(arr)) {
     return "It's a fake array";
@@ -16792,3 +16793,48 @@ function blackAndWhite(arr) {
 console.log(blackAndWhite(5, 13));
 console.log(blackAndWhite([5, 13]));
 console.log(blackAndWhite([5, 12]));
+*/
+
+// #3
+/*
+In this Kata, you will be given a series of times at which an alarm sounds. Your task will be to determine the maximum time interval between alarms. Each alarm starts ringing at the beginning of the corresponding minute and rings for exactly one minute. The times in the array are not in chronological order. Ignore duplicate times, if any.
+
+For example:
+solve(["14:51"]) = "23:59". If the alarm sounds now, it will not sound for another 23 hours and 59 minutes.
+solve(["23:00","04:22","18:05","06:24"]) == "11:40". The max interval that the alarm will not sound is 11 hours and 40 minutes.
+
+In the second example, the alarm sounds 4 times in a day.
+
+More examples in test cases. Good luck!
+*/
+function solve(arr) {
+  var sortedAsMinutes = arr.map((x) => timeToMinutes(x)).sort((a, b) => a - b);
+  sortedAsMinutes.push(sortedAsMinutes[0] + 60 * 24);
+  return minutesToTime(
+    Math.max.apply(
+      null,
+      sortedAsMinutes.map((x, i, a) => {
+        return i < a.length - 1 ? a[i + 1] - x : 0;
+      })
+    ) - 1
+  );
+}
+
+function timeToMinutes(time) {
+  var tokens = time.split(":");
+  return parseInt(tokens[0], 10) * 60 + parseInt(tokens[1], 10);
+}
+
+function minutesToTime(minutes) {
+  return (
+    ("" + Math.floor(minutes / 60)).padStart(2, "0") +
+    ":" +
+    ("" + (minutes % 60)).padStart(2, "0")
+  );
+}
+
+// Примеры вызова
+console.log(solve(["14:51"])); // "23:59"
+console.log(solve(["23:00", "04:22", "18:05", "06:24"])); // "11:40"
+console.log(solve(["5:22", "13:43", "12:19"])); // "6:21"
+console.log(solve(["23:00", "23:59", "00:00"])); // "23:59"
