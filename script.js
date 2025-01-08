4ab7b412eb1e5f60bd69481d02fd8b176189bdc7
DmitriyAngve <kirirch129@gmail.com>
2025-01-08 15:39:44 +0300

    strSize

--- script.js
+++ script.js
@@ -16852,6 +16852,7 @@ a and b are integers and > 0
 
 Write a function which returns the area of the arrow.
 */
+/*
 function arrowArea(a, b) {
   let gip = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)).toFixed(2);
   gip = Number(gip / 2);
@@ -16860,3 +16861,31 @@ function arrowArea(a, b) {
 }
 
 console.log(arrowArea(7, 6));
+*/
+
+// #2
+/*
+Create a function that will trim a string (the first argument given) if it is longer than the requested maximum string length (the second argument given). The result should also end with "..."
+
+These dots at the end also add to the string length.
+
+For example, trim("Creating kata is fun", 14) should return "Creating ka..."
+
+If the string is smaller or equal than the maximum string length, then simply return the string with no trimming or dots required.
+
+e.g. trim("Code Wars is pretty rad", 50) should return "Code Wars is pretty rad"
+
+If the requested string length is smaller than or equal to 3 characters, then the length of the dots is not added to the string length.
+
+e.g. trim("He", 1) should return "H...", because 1 <= 3
+
+Requested maximum length will be greater than 0. Input string will not be empty
+*/
+function trim(str, size) {
+  if (str.length <= size) return str;
+  if (size <= 3) return str.slice(0, size) + "...";
+  return str.slice(0, size - 3) + "...";
+}
+
+console.log(trim("Creating kata is fun", 14));
+console.log(trim("He", 1));
