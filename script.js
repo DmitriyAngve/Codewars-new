638fb53b697860eb9655f18fe8d6f0fa1d8fa2a8
DmitriyAngve <kirirch129@gmail.com>
2025-01-14 16:20:31 +0300

    oddOnesOut

--- script.js
+++ script.js
@@ -17474,6 +17474,7 @@ Word values
 
 Non-even substrings
 */
+/*
 function solve(a, b) {
   let res = [];
   for (let i = 0; i < b.length; i++) {
@@ -17490,3 +17491,48 @@ function solve(a, b) {
 }
 
 console.log(solve(["abc", "abc", "xyz", "cde", "uvw"], ["abc", "cde", "uap"]));
+*/
+
+// #5
+/*
+The town sheriff dislikes odd numbers and wants all odd numbered families out of town! In town crowds can form and individuals are often mixed with other people and families. However you can distinguish the family they belong to by the number on the shirts they wear. As the sheriff's assistant it's your job to find all the odd numbered families and remove them from the town!
+
+Challenge: You are given a list of numbers. The numbers each repeat a certain number of times. Remove all numbers that repeat an odd number of times while keeping everything else the same.
+
+oddOnesOut([1, 2, 3, 1, 3, 3]) = [1, 1]
+
+In the above example:
+
+    the number 1 appears twice
+    the number 2 appears once
+    the number 3 appears three times
+
+2 and 3 both appear an odd number of times, so they are removed from the list. The final result is: [1,1]
+
+Here are more examples:
+
+oddOnesOut([1, 1, 2, 2, 3, 3, 3]) = [1, 1, 2, 2]
+oddOnesOut([26, 23, 24, 17, 23, 24, 23, 26]) = [26, 24, 24, 26]
+oddOnesOut([1, 2, 3]) = []
+oddOnesOut([1]) = []
+
+Are you up to the challenge?
+*/
+function oddOnesOut(nums) {
+  let ht = {};
+
+  for (const num of nums) {
+    ht[num] = (ht[num] || 0) + 1;
+  }
+  let res = [];
+  for (const num of nums) {
+    if (ht[num] % 2 === 0) {
+      res.push(num);
+
+      ht[num] = 0;
+    }
+  }
+  return res;
+}
+
+console.log(oddOnesOut([1, 2, 3, 1, 3, 3]));
