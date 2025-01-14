b692ba002906c04fc90ff7bbae71c1c58e6038b2
DmitriyAngve <kirirch129@gmail.com>
2025-01-14 14:44:26 +0300

    stringmatchup

--- script.js
+++ script.js
@@ -17422,6 +17422,7 @@ countZeros(10); // returns 1
 countZeros(100); // returns 11
 countZeros(200); // returns 31
 */
+/*
 function countZeros(n) {
   // let zerosCount = 0;
   // for (let i = n; i > 0; i--) {
@@ -17447,3 +17448,45 @@ function countZeros(n) {
 console.log(countZeros(10));
 console.log(countZeros(100));
 console.log(countZeros(200));
+*/
+
+// #4
+/*
+Given two arrays of strings, return the number of times each string of the second array appears in the first array.
+Example
+
+array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
+array2 = ['abc', 'cde', 'uap']
+
+How many times do the elements in array2 appear in array1?
+
+    'abc' appears twice in the first array (2)
+    'cde' appears only once (1)
+    'uap' does not appear in the first array (0)
+
+Therefore, solve(array1, array2) = [2, 1, 0]
+
+Good luck!
+
+If you like this Kata, please try:
+
+Word values
+
+Non-even substrings
+*/
+function solve(a, b) {
+  let res = [];
+  for (let i = 0; i < b.length; i++) {
+    let count = 0;
+    for (let j = 0; j < a.length; j++) {
+      console.log(b[i]);
+      if (b[i] === a[j]) {
+        count++;
+      }
+    }
+    res.push(count);
+  }
+  return res;
+}
+
+console.log(solve(["abc", "abc", "xyz", "cde", "uvw"], ["abc", "cde", "uap"]));
