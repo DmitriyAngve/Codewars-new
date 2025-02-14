c138b8483abadaeeec54bdee12ab99261277df44
DmitriyAngve <kirirch129@gmail.com>
2025-02-14 17:44:58 +0300

    converter

--- script.js
+++ script.js
@@ -18935,6 +18935,7 @@ If a = [1, 2] and b = [1], the result should be [2].
 
 If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
 */
+/*
 function arrayDiff(a, b) {
   let res = [];
   for (let i = 0; i < a.length; i++) {
@@ -18953,3 +18954,26 @@ function arrayDiff(a, b) {
 }
 
 console.log(arrayDiff([1, 2, 2, 2, 3], [1, 2]));
+*/
+
+// # 14.02.2025
+// #1
+/*
+Sometimes, I want to quickly be able to convert miles per imperial gallon (mpg) into kilometers per liter (kpl).
+
+Create an application that will display the number of kilometers per liter (output) based on the number of miles per imperial gallon (input).
+
+Your answer should be accurate within 0.01 kpl.
+
+Some useful associations relevant to this kata:
+
+    1 Imperial Gallon = 4.54609188 litres
+    1 Mile = 1.609344 kilometres
+*/
+
+function converter(mpg) {
+  return Math.round(((mpg * 1.609344) / 4.54609188) * 100) / 100;
+}
+
+console.log(converter(10));
+console.log(converter(30));
