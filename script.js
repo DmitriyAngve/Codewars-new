a6ca5e0b7095afec4cf22780f33660e349f8c99f
DmitriyAngve <kirirch129@gmail.com>
2025-02-18 13:21:58 +0300

    calculateExp

--- script.js
+++ script.js
@@ -19178,8 +19178,64 @@ Examples
   5  -->  0 (false) because none of the odd bits are 1 (5 = 0b101)
 170  -->  1 (true) because all of the odd bits are 1 (170 = 0b10101010)
 */
+/*
 function anyOdd(x) {
   return (x & 0xaaaaaaaa) !== 0 ? 1 : 0;
 }
 
 console.log(anyOdd(170));
+*/
+
+// #2
+/*
+Your task is to write a function calculate, which accepts a string with a mathematical exprssion written in prefix Polish notation and evaluates it. This means that all operations are placed before their operands. For example, the expression 3 + 5 is written in Polish notation as + 3 5, and (3 + 5) / (2 * 2) is / + 3 5 * 2 2.
+
+The only valid operations are +, -, * and /. The input string is guaranteed to be a valid expression.
+
+You can use eval or alternative if available in your language, but it is in no way needed for an idiomatic solution.
+Examples
+
+calculate('123.456')       == 123.456
+calculate('+ -5 5')        == 0
+calculate('* + 2 2 3')     == 12
+calculate('/ + 3 5 * 2 2') == 2
+
+Input
+
+A non-empty string consisting of numbers and arithmetic operators separated by spaces. This string is a valid arithmetic expression written in prefix polish notation.
+Output
+
+A number, result of evaluating the expression.
+*/
+function calculate(expression) {
+  const tokens = expression.split(" ").reverse();
+  const stack = [];
+
+  for (const token of tokens) {
+    if (!isNaN(token)) {
+      stack.push(Number(token));
+    } else {
+      const a = stack.pop();
+      const b = stack.pop();
+      switch (token) {
+        case "+":
+          stack.push(a + b);
+          break;
+        case "-":
+          stack.push(a - b);
+          break;
+        case "*":
+          stack.push(a * b);
+          break;
+        case "/":
+          stack.push(a / b);
+          break;
+      }
+    }
+  }
+
+  return stack[0];
+}
+
+console.log(calculate("+ -5 5"));
+console.log(calculate("* + 2 2 3"));
