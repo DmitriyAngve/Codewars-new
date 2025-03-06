169ba9b54d68a0a72bfe000cf92f4bdb659c9500
DmitriyAngve <kirirch129@gmail.com>
2025-03-06 15:57:33 +0300

    divisibleBy

--- script.js
+++ script.js
@@ -19438,6 +19438,7 @@ The following was a question that I received during a technical interview for an
 
 You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
 */
+/*
 function missingNo(nums) {
   const n = 100;
   const expected = (n * (n + 1)) / 2;
@@ -19458,3 +19459,17 @@ console.log(
     61, 99, 16,
   ])
 );
+*/
+
+// #4
+/*
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+Example(Input1, Input2 --> Output)
+
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+*/
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((num) => num % divisor === 0);
+}
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2));
