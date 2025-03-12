a40f8a2dae42d985367e34bffcb09deb775cc743
DmitriyAngve <kirirch129@gmail.com>
2025-03-12 15:53:44 +0300

    doubles

--- script.js
+++ script.js
@@ -19525,6 +19525,7 @@ Write a function that replaces 'two', 'too' and 'to' with the number '2'. Even i
 
 Note that 'too' should become '2', not '2o'
 */
+/*
 function textin(s) {
   return s.replace(/(two|too|to)/gi, "2");
 }
@@ -19533,3 +19534,39 @@ console.log(textin("see you tomorrow"));
 console.log(textin("look at that octopus"));
 console.log(textin("look at that octwopus"));
 console.log(textin("look at that octoopus"));
+*/
+
+// #2
+/*
+In this Kata, you will write a function doubles that will remove double string characters that are adjacent to each other.
+
+For example:
+
+doubles('abbcccdddda') = 'aca', because, from left to right:
+
+a) There is only one 'a' on the left hand side, so it stays.
+b) The 2 b's disappear because we are removing double characters that are adjacent. 
+c) Of the 3 c's, we remove two. We are only removing doubles. 
+d) The 4 d's all disappear, because we first remove the first double, and again we remove the second double.
+e) There is only one 'a' at the end, so it stays.
+
+Two more examples: doubles('abbbzz') = 'ab' and doubles('abba') = "". In the second example, when we remove the b's in 'abba', the double a that results is then removed.
+
+The strings will contain lowercase letters only. More examples in the test cases.
+
+Good luck!
+*/
+function doubles(s) {
+  let res = [];
+  for (let char of s) {
+    if (res.length && res[res.length - 1] === char) {
+      res.pop();
+    } else {
+      res.push(char);
+    }
+  }
+  return res.join("");
+}
+
+console.log(doubles("abbbzz"));
+console.log(doubles("xxbnnnnnyaaaaam"));
