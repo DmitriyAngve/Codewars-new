a31f4b9c702f229f2ea9966ae464347baad3daec
DmitriyAngve <kirirch129@gmail.com>
2025-04-01 17:06:51 +0300

    orderBreaker

--- script.js
+++ script.js
@@ -20084,6 +20084,7 @@ func myFirstKata(a b) {
   )
 }
 */
+/*
 function myFirstKata(a, b) {
   if (typeof a !== "number" || typeof b !== "number") {
     return false;
@@ -20093,3 +20094,60 @@ function myFirstKata(a, b) {
 }
 console.log(myFirstKata(4, 2));
 console.log(myFirstKata(4, "!"));
+*/
+
+// #10
+/*
+In this kata, you have an integer array which was ordered by ascending except one number.
+
+For Example: [1,2,3,4,17,5,6,7,8]
+For Example: [19,27,33,34,112,578,116,170,800]
+
+You need to figure out the first breaker. Breaker is the item, when removed from sequence, sequence becomes ordered by ascending.
+
+For Example: [1,2,3,4,17,5,6,7,8] => 17 is the only breaker.
+
+For Example: [19,27,33,34,112,578,116,170,800] => 578 is the only breaker.
+
+For Example: [105, 110, 111, 112, 114, 113, 115] => 114 and 113 are breakers. 114 is the first breaker.
+    When removed 114, sequence becomes ordered by ascending => [105, 110, 111, 112, 113, 115]
+    When removed 113, sequence becomes ordered by ascending => [105, 110, 111, 112, 114, 115]
+
+For Example: [1, 0, 2] => 1 and 0 are the breakers. 1 is the first breaker.
+    When removed 1, sequence becomes ordered by ascending => [0, 2]
+    When removed 0, sequence becomes ordered by ascending => [1, 2]
+    
+For Example: [1, 2, 0, 3, 4] => 0 is the only breaker.
+    When removed 0, sequence becomes ordered by ascending. => [1, 2, 3, 4]
+    
+
+TASK:
+Write a function that returns the first breaker.
+
+Notes:
+
+    Input array does not contain any duplicate element.
+*/
+function orderBreaker(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const testArr = arr.slice(0, i).concat(arr.slice(i + 1));
+
+    let isSorted = true;
+    for (let j = 1; j < testArr.length; j++) {
+      if (testArr[j] < testArr[j - 1]) {
+        isSorted = false;
+        break;
+      }
+    }
+    if (isSorted) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+console.log(orderBreaker([1, 2, 3, 4, 17, 5, 6, 7, 8]));
+// console.log(orderBreaker([19, 27, 33, 34, 112, 578, 116, 170, 800]));
+// console.log(orderBreaker([105, 110, 111, 112, 114, 113, 115]));
+// console.log(orderBreaker([2, 0, 15, 3, 1, 2]));
+// console.log(orderBreaker([903225, 882792, 886075, 293791]));
