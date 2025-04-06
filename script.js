c0f432970cbc3837a2f35f418d570dd878a400fa
DmitriyAngve <kirirch129@gmail.com>
2025-04-06 11:51:56 +0300

    isAllPossibilities

--- script.js
+++ script.js
@@ -20245,6 +20245,7 @@ new       => ["a", "c"]
 output:
 1 * 2 + 2 + 3 * 2 = 10
 */
+/*
 function maxPossibleScore(obj, arr) {
   let res = 0;
 
@@ -20264,3 +20265,29 @@ function multy(val) {}
 
 console.log(maxPossibleScore({ "a": 1, "b": 2, "c": 4 }, ["a", "b", "c"]));
 console.log(maxPossibleScore({ "a": 1, "b": 14 }, ["b"]));
+*/
+
+// 06.04.2025
+
+/*
+A non-empty array a of length n is called an array of all possibilities if it contains all numbers between 0 and a.length - 1 (both inclusive).
+
+Write a function that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+Examples:
+
+[1,2,0,3] => True
+# Includes all numbers between 0 and a.length - 1 (4 - 1 = 3)
+
+[0,1,2,2,3] => False
+# Doesn't include all numbers between 0 and a.length - 1 (5 - 1 = 4)
+
+[0] => True
+# Includes all numbers between 0 and a.length - 1 (1 - 1 = 0).
+*/
+function isAllPossibilities(x) {
+  false;
+}
+
+console.log(isAllPossibilities([0, 1, 2, 3]));
+console.log(isAllPossibilities([1, 2, 3, 4]));
