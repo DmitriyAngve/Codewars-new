6154d34a110091a55146ebdad4d624d8a51ca22e
DmitriyAngve <kirirch129@gmail.com>
2025-04-14 11:40:38 +0300

    filter_list

--- script.js
+++ script.js
@@ -20451,7 +20451,7 @@ const userPermissions = ['read', 'delete', 'write'];
 hasAllPermissions(['read', 'write'], ['read', 'write']);      // true
 hasAllPermissions(['read', 'delete'], ['read', 'write']);     // false
 */
-
+/*
 function hasAllPermissions(userPermissions, requiredPermissions) {
   let count = 0;
 
@@ -20467,3 +20467,22 @@ function hasAllPermissions(userPermissions, requiredPermissions) {
 
 console.log(hasAllPermissions(["read", "write"], ["read", "write"]));
 console.log(hasAllPermissions(["read", "delete"], ["read", "write"]));
+*/
+
+// 14.04.2025
+// #1
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+Example
+
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+*/
+function filter_list(l) {
+  return l.filter((el) => typeof el === "number");
+}
+
+console.log(filter_list([1, "a", "b", 0, 15]));
+console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
+console.log(filter_list([1, 2, "a", "b"]));
