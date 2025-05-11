ffbc00110ed6fa62fd90e709704d71e2f1c58c30
DmitriyAngve <kirirch129@gmail.com>
2025-05-11 18:22:40 +0300

    scrabbleScore

--- script.js
+++ script.js
@@ -20654,6 +20654,7 @@ Examples (input -> output)
     "kata" -> false ('a' comes after 'k')
     "ant" -> true (all characters are in alphabetical order)
 */
+/*
 function alphabetic(s) {
   let res = s.split("").map((el) => el.charCodeAt(0) - 96);
   console.log(res);
@@ -20664,3 +20665,75 @@ function alphabetic(s) {
 
 console.log(alphabetic("kata"));
 console.log(alphabetic("ant"));
+*/
+
+// 11.05.2025
+// #1
+/*
+Write a program that, given a word, computes the scrabble score for that word.
+Letter Values
+
+You'll need these:
+
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+
+There will be a preloaded hashtable $dict with all these values: $dict["E"] == 1.
+Examples
+
+"cabbage" --> 14
+
+"cabbage" should be scored as worth 14 points:
+
+    3 points for C
+    1 point for A, twice
+    3 points for B, twice
+    2 points for G
+    1 point for E
+
+And to total:
+
+3 + 2*1 + 2*3 + 2 + 1 = 3 + 2 + 6 + 3 = 14
+
+Empty string should return 0. The string can contain spaces and letters (upper and lower case), you should calculate the scrabble score only of the letters in that string.
+
+""           --> 0
+"STREET"    --> 6
+"st re et"    --> 6
+"ca bba g  e" --> 14
+*/
+
+function scrabbleScore(str) {
+  let score = 0;
+
+  const values = Object.fromEntries(
+    [
+      [1, "AEIOULNRST"],
+      [2, "DG"],
+      [3, "BCMP"],
+      [4, "FHVWY"],
+      [5, "K"],
+      [8, "JX"],
+      [10, "QZ"],
+    ].flatMap(([value, letters]) =>
+      letters.split("").map((letter) => [letter, value])
+    )
+  );
+  console.log(values);
+
+  for (let char of str.toUpperCase()) {
+    if (values[char]) {
+      score += values[char];
+    }
+  }
+
+  return score;
+}
+
+console.log(scrabbleScore("cabbage"));
