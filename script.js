18e9842784f4a167ea20cc5dee0544c6ee54608a
DmitriyAngve <kirirch129@gmail.com>
2025-06-04 12:45:39 +0300

    isCleanlyNested

--- script.js
+++ script.js
@@ -20708,7 +20708,7 @@ Empty string should return 0. The string can contain spaces and letters (upper a
 "st re et"    --> 6
 "ca bba g  e" --> 14
 */
-
+/*
 function scrabbleScore(str) {
   let score = 0;
 
@@ -20737,3 +20737,70 @@ function scrabbleScore(str) {
 }
 
 console.log(scrabbleScore("cabbage"));
+*/
+
+// 04.05.2025
+// #1
+/*
+Clean Nesting Tree
+
+Task:
+
+You are given a main list that may contain nested lists of arbitrary depth. Imagine this structure as a tree, where each list is a node and its elements are child nodes.
+
+Your task is to implement a function that checks whether the entire structure follows the nesting rule:
+
+At each level (depth) of the tree, each node's children must all either be:
+
+    dead ends (empty lists [])
+
+    nested lists
+
+Examples:
+
+    [ [ [[]], [[]], [[]]  ], [[]] , [[]]  ] -> True
+
+    [] -> True
+
+    [ [], [] ] -> True
+
+    [ [ [], [], []  ] , [ [], [[]]  ] ] -> False
+
+Explanation:
+
+look at the array at index 1 in the main array [ [], [[]]  ] . There are 2 arrays in it [], [[]]. One nested and one deadend. This violates the nesting rule
+
+    [ [ [ [[]],[[]]  ], [[]] ] , [ [] ] ] -> True
+
+    [ [ [ [[]], [[]], []  ], [[]] ] , [ [] ] ] -> False
+
+Explanation:
+
+first 2 arrays in the main list are both nested, so they are okay. We then check the first one at index 0 [ [ [[]], [[]], []  ], [[]] ]. It has 2 arrays: [ [[]], [[]], []  ] and [[]]. Both nested, so they are fine. We go deeper into the first one out of these two. It has three arrays: [[]], [[]], []. This violates the rule because first two are nested but the last one is deadend. They must be all either nested or deadends.
+
+    [ [], [[], [], []] , [] ] -> False
+
+Explanation:
+
+there are three arrays in the main array [], [[], [], []] , []. 2 are dead ends but one is nested, which is inconsistent.
+*/
+function isCleanlyNested(xs) {
+  if (!Array.isArray(xs)) return false;
+
+  if (xs.length === 0) return true;
+
+  const allEmpty = xs.every((x) => Array.isArray(x) && x.length === 0);
+  const allNested = xs.every((x) => Array.isArray(x) && x.length > 0);
+
+  if (!allEmpty && !allNested) return false;
+
+  return allNested ? xs.every(isCleanlyNested) : true;
+}
+
+console.log(isCleanlyNested([[[[]], [[]], [[]]], [[]], [[]]]));
+console.log(
+  isCleanlyNested([
+    [[], [], []],
+    [[], [[]]],
+  ])
+);
