d780ce970cd63b3080f7dd23c9fb36c5d9098fac
DmitriyAngve <kirirch129@gmail.com>
2025-06-06 13:29:52 +0300

    isSquare

--- script.js
+++ script.js
@@ -20901,6 +20901,7 @@ Assume input is array.
 
 Hint: Math.random() 
 */
+/*
 function shuffle(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
@@ -20911,3 +20912,49 @@ function shuffle(arr) {
 }
 
 console.log(shuffle([1, 2, 3, 4]));
+*/
+
+// #2
+/*
+Another Kata testing an whether all elements of an array are square, although this time the array can be a multidemtional array with any number of dimensions.
+
+Your task - Write a function that checks whether all elements in a multidimensional array are square numbers. The function should be able to take any number of array elements and any number of dimensions.
+
+Your function should return true if all elements are square numbers and false if not.
+
+An entirely empty array should return undefined.
+
+You can assume that all array elements will be positive integers.
+
+Examples:
+
+isSquare([1, 4, 9, 16, 25, 36]);
+//returns true
+
+isSquare([1, 2, 3, 4, 5, 6]);
+//returns false
+
+isSquare([1, [4], [9, 16, 25], [36, 49, [64, 81]], [100, [121, 144, [169]]], [196, [225, [256, 289, [324, [361, 400]]]]]]);
+//returns true
+*/
+var isSquare = function (arr) {
+  const flatArr = arr.flat(Infinity);
+  console.log(flatArr);
+
+  if (flatArr.length === 0) return undefined;
+
+  return flatArr.every((n) => Number.isInteger(Math.sqrt(n)));
+};
+
+console.log(isSquare([1, 2, 3, 4, 5, 6]));
+console.log(isSquare([1, 4, 9, 16, 25, 36]));
+console.log(
+  isSquare([
+    1,
+    [4],
+    [9, 16, 25],
+    [36, 49, [64, 81]],
+    [100, [121, 144, [169]]],
+    [196, [225, [256, 289, [324, [361, 400]]]]],
+  ])
+);
