6b7c3beddb6171a3cdf33d367f22adbd5daa0cde
DmitriyAngve <kirirch129@gmail.com>
2025-06-20 17:16:00 +0300

    largestPairSum

--- script.js
+++ script.js
@@ -21160,6 +21160,7 @@ A building begins with a strength value of 1000 when first built, but this value
 
 Given an earthquake and the age of a building, write a function that returns "Safe!" if the building is strong enough, or "Needs Reinforcement!" if it falls.
 */
+/*
 function strongEnough(arr, age) {
   let magnitude = [];
 
@@ -21186,3 +21187,24 @@ console.log(
     2
   )
 );
+*/
+
+// #6
+/*
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+
+Input sequence contains minimum two elements and every element is an integer.
+*/
+function largestPairSum(numbers) {
+  let sorted = numbers.sort((a, b) => b - a);
+
+  return sorted[0] + sorted[1];
+}
+
+console.log(largestPairSum([10, 14, 2, 23, 19]));
+console.log(largestPairSum([99, 2, 2, 23, 19]));
