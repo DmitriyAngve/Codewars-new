31e3703599c7de043a9e8ef39e6bcc5855df7f2f
DmitriyAngve <kirirch129@gmail.com>
2025-06-20 16:15:53 +0300

    isTriangle2

--- script.js
+++ script.js
@@ -21069,6 +21069,7 @@ grouped[3]     // [35, 38]
 grouped[4]     // undefined
 grouped[5]     // [50]
 */
+/*
 function groupIn10s(...nums) {
   const res = [];
 
@@ -21085,3 +21086,33 @@ function groupIn10s(...nums) {
 }
 
 console.log(groupIn10s(8, 12, 38, 3, 17, 19, 25, 35, 50));
+*/
+
+// #3
+/*
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+Examples:
+
+Input -> Output
+1,2,2 -> true
+4,2,3 -> true
+2,2,2 -> true
+1,2,3 -> false
+-5,1,3 -> false
+0,2,3 -> false
+1,2,9 -> false 
+*/
+function isTriangle(a, b, c) {
+  if (a + b > c && a + c > b && b + c > a && (a > 0 && b > 0, c > 0)) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isTriangle(1, 2, 2));
+console.log(isTriangle(4, 2, 3));
+console.log(isTriangle(1, 2, 3));
+console.log(isTriangle(-1, 2, 3));
