b97cfa05d50e475f04a851ab4194f3cc37c6aa77
DmitriyAngve <kirirch129@gmail.com>
2025-06-23 16:08:56 +0300

    regexMatches

--- script.js
+++ script.js
@@ -21587,6 +21587,7 @@ add = 1
 
 --> 0.6666666666666666666666666666666666 // (2/3)
 */
+/*
 function addToAverage(current, points, add) {
   const newAvg = (current * points + add) / (points + 1);
   return newAvg;
@@ -21594,3 +21595,42 @@ function addToAverage(current, points, add) {
 
 console.log(addToAverage(0, 0, 1));
 console.log(addToAverage(1, 1, 2));
+*/
+
+// #2
+/*
+In regular expression, there is a meta-character called alternation, written as |. This meta-character behaves like an OR operator that matches either of the sides.
+
+For example: abc|123 will match either "abc" or "123". They can also match empty string.
+Task
+
+Write a function called regexMatches(regex). Given a regex pattern, return a list of all possible matches.
+Examples
+
+let pattern1 = = /abc|123/
+regexMatches(pattern1) # -> should output ["abc", "123"]
+
+let pattern2 = = /abc|def|ghi/
+regexMatches(pattern2) # -> should output ["abc", "def", "ghi"]
+
+let pattern3 = = /1|2|3|4|5/
+regexMatches(pattern3) # -> should output ["1", "2", "3", "4", "5"]
+
+let pattern4 = = /a/
+regexMatches(pattern3) # -> should output ["a"]
+
+let pattern5 = = /|empty/
+regexMatches(pattern3) # -> should output ["", "empty"]
+
+Hint
+
+You can use regex.source to get the string representation of the regex.
+Input
+
+The regex pattern will only consist of digits, lowercase letters, and the alternation meta-character |. The alternation can be at the very start or at the very end.
+*/
+function regexMatches(regex) {
+  return regex.source.split("|");
+}
+
+console.log(regexMatches(/abc|123/));
