033833e70af3dbaad02d0c69a0fbcf40100a43de
DmitriyAngve <kirirch129@gmail.com>
2025-06-26 15:01:34 +0300

    isSquare2

--- script.js
+++ script.js
@@ -22056,6 +22056,7 @@ Write a function that takes an integer as input, and returns the number of bits 
 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 */
+/*
 function countBits(n) {
   const bin = n
     .toString(2)
@@ -22067,3 +22068,44 @@ function countBits(n) {
 console.log(countBits(0));
 console.log(countBits(4));
 console.log(countBits(10));
+*/
+
+// #6
+/*
+A square of squares
+
+You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+Task
+
+Given an integral number, determine if it's a square number:
+
+    In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+Examples
+
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+*/
+var isSquare = function (n) {
+  if (n < 0) return false;
+  if (n === 0) return true;
+  let arr = Math.sqrt(n).toString().split("").join("");
+
+  let res = arr.replace(/[.]/g, "");
+  return arr.length <= res.length;
+  // return Number.isInteger(Math.sqrt(n));
+};
+
+console.log(isSquare(-1));
+console.log(isSquare(0));
+console.log(isSquare(3));
+console.log(isSquare(4));
+console.log(isSquare(25));
+console.log(isSquare(26));
