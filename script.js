812828e94e9392092526b791c5d08c6a801980c8
DmitriyAngve <kirirch129@gmail.com>
2025-06-27 16:57:58 +0300

    createPhoneNumber

--- script.js
+++ script.js
@@ -22582,6 +22582,7 @@ is_square([3, 4, 7, 9]) --> False
 
 is_square([]) --> None
 */
+/*
 var isSquare = function (arr) {
   if (!arr.length) return undefined;
   for (let i = 0; i < arr.length; i++) {
@@ -22596,3 +22597,25 @@ console.log(isSquare([1, 4, 9, 16]));
 console.log(isSquare([3, 4, 7, 9]));
 console.log(isSquare([]));
 console.log(isSquare([1, 2, 4, 15]));
+*/
+
+// #11
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+Example
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+
+*/
+function createPhoneNumber(numbers) {
+  let first = [numbers[0], numbers[1], numbers[2]].join("");
+  let second = [numbers[3], numbers[4], numbers[5]].join("");
+  let third = [numbers[6], numbers[7], numbers[8], numbers[9]].join("");
+  return `(${first}) ${second}-${third}`;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
