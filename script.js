4bf30dd85396dc99bf3bc21e625711e1c502323d
DmitriyAngve <kirirch129@gmail.com>
2025-07-02 13:20:58 +0300

    vaporcode

--- script.js
+++ script.js
@@ -22925,7 +22925,7 @@ Examples:
   [ '', 'a', '', '', '', '', '', '' ]
 ] // => "The Mitochondria is the powerhouse of the cell"
 */
-
+/*
 function arrAdder(arr) {
   const flatArr = arr.flat(Infinity);
   const rows = arr.length; // сколько букв в одном слове
@@ -22953,3 +22953,30 @@ console.log(
     ["t", "e", "e", ""],
   ])
 );
+*/
+
+// 02.07.2025
+// #1
+/*
+ASC Week 1 Challenge 4 (Medium #1)
+
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+Note that spaces should be ignored in this case.
+Examples
+
+"Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+"Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+*/
+function vaporcode(str) {
+  let arr = str.replace(/ /g, "").split("");
+  // console.log(arr);
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    result += `${arr[i].toUpperCase()}  `;
+  }
+  return result.trim();
+}
+
+console.log(vaporcode("Why isn't my code working?"));
+console.log(vaporcode("Lets go to the movies"));
