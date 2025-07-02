68751358811e883f5d9a6dc8b820a7a1ee67bb52
DmitriyAngve <kirirch129@gmail.com>
2025-07-02 14:42:58 +0300

    compute

--- script.js
+++ script.js
@@ -23100,6 +23100,7 @@ If any input is an empty string, it should return the number that isn't just an 
 
 HINT: Remember Number()/parseInt() from https://www.codewars.com/kata/5966e33c4e686b508700002d
 */
+/*
 function sumArr(a, b) {
   let res = [];
   for (let i = 0; i < a.length; i++) {
@@ -23120,3 +23121,34 @@ function sumArr(a, b) {
 }
 
 console.log(sumArr(["4", "5", "6", "7", "8"], ["1", "2", "3", "4", "5"]));
+*/
+
+// #6
+/*
+This Kata is intended as a small challenge for my students
+
+Create a function, called compute, that accepts an integer argument, N, and returns the value of the following expression, where N is a odd positive integer given by the user
+
+Expression : 1 +....+ 1/(N)^2
+
+For example, when N = 9, compute() should evaluate the following: 1 + 1/(3)^2 + 1/(5)^2 + 1/(7)^2 + 1/(9)^2 == ~1.1838649533887629
+
+Note: User input of an even number OR a number less than 3 should return 0.
+
+compute(5); // => 1.1511111111111112
+compute(9); // => 1.1838649533887629
+compute(-1); // => 0
+compute(8); // => 0
+*/
+function compute(n) {
+  if (n < 3 || n % 2 === 0) return 0;
+  let sum = 1;
+  for (let i = 3; i <= n; i += 2) {
+    sum += 1 / (i * i);
+  }
+  return sum;
+}
+
+console.log(compute(5));
+console.log(compute(0));
+console.log(compute(2));
