80bdbb9d78b87a6d382675f8a59d774c73e40548
DmitriyAngve <kirirch129@gmail.com>
2025-07-03 14:48:13 +0300

    formatWith

--- script.js
+++ script.js
@@ -23191,6 +23191,7 @@ XO("ooxXm") => true
 XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 XO("zzoo") => false
 */
+/*
 function XO(str) {
   str = str.toLowerCase();
   let xCount = 0;
@@ -23210,3 +23211,27 @@ function XO(str) {
 console.log(XO("ooxx"));
 console.log(XO("zpzpzpp"));
 console.log(XO("xooxx"));
+*/
+
+// 03.07.2025
+// #1
+/*
+Write
+
+String.prototype.formatWith = function () {}
+
+that takes in a list of arguments and formats the string based off these arguments and indices.
+
+For example:
+
+'Hello {0}'.formatWith('Codewars') ===  'Hello Codewars'
+'{0} + {0} = {1}'.formatWith('1','2') ===  '1 + 1 = 2'
+'Testing {1}'.formatWith('kata') === 'Testing {1}' // there is no arguments at position 1
+*/
+String.prototype.formatWith = function (...args) {
+  return this.replace(/{(\d+)}/g, (match, index) => {
+    return args[index] !== undefined ? args[index] : match;
+  });
+};
+
+console.log("Hello {0}".formatWith("Codewars"));
