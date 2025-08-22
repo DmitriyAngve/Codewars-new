98b6dc6af1fa2ba2f8bd298f22b98f550da90208
DmitriyAngve <kirirch129@gmail.com>
2025-08-22 15:13:58 +0300

    sumOfDifferences1

--- script.js
+++ script.js
@@ -25135,6 +25135,7 @@ Input: 145263 Output: 654321
 
 Input: 123456789 Output: 987654321
 */
+/*
 function descendingOrder(n) {
   return Number(
     n
@@ -25146,3 +25147,30 @@ function descendingOrder(n) {
   );
 }
 console.log(descendingOrder(42145));
+*/
+
+// #3
+/*
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+Example
+
+[2, 1, 10]  -->  9
+
+In descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+*/
+function sumOfDifferences(arr) {
+  if (!arr.length) return 0;
+  let sorted = arr.sort((a, b) => b - a);
+
+  let res = 0;
+  for (let i = 0; i < sorted.length - 1; i++) {
+    res += sorted[i] - sorted[i + 1];
+  }
+  return res;
+}
+
+console.log(sumOfDifferences([2, 1, 10]));
