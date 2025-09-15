71b6531556a2e65da2b27e46d55c2ff00d730d0f
DmitriyAngve <kirirch129@gmail.com>
2025-09-15 12:30:21 +0300

    mergeArrays2

--- script.js
+++ script.js
@@ -25431,6 +25431,7 @@ Example 2
 
 There is nothing between -1 and -2, so return 0.
 */
+/*
 function maxSumBetweenTwoNegatives(a) {
   let indexes = negInd(a);
 
@@ -25468,3 +25469,34 @@ function negInd(arr) {
 console.log(maxSumBetweenTwoNegatives([4, -1, 6, -2, 3, 5, -7, 7]));
 console.log(maxSumBetweenTwoNegatives([4, -1, -2]));
 console.log(maxSumBetweenTwoNegatives([4, -1, 2]));
+*/
+
+// #3
+/*
+You are given two sorted arrays that contain only integers. These arrays may be sorted in either ascending or descending order. Your task is to merge them into a single array, ensuring that:
+
+    The resulting array is sorted in ascending order.
+
+    Any duplicate values are removed, so each integer appears only once.
+
+    If both input arrays are empty, return an empty array.
+
+No input validation is needed, as both arrays are guaranteed to contain zero or more integers.
+Examples (input -> output)
+
+* [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+*/
+function mergeArrays(arr1, arr2) {
+  if (!arr1.length && !arr2.length) return [];
+  let sorted = arr1.concat(arr2).sort((a, b) => a - b);
+
+  return Array.from(new Set(sorted));
+}
+
+console.log(mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]));
+console.log(mergeArrays([1, 2, 5, 7, 9], [10, 8, 6, 4, 2]));
+console.log(mergeArrays([], []));
