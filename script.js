5a781fe6a0b2c47f1148c2fd1ff92e86ea2ce46b
DmitriyAngve <kirirch129@gmail.com>
2025-10-06 13:07:26 +0300

    nextVersion

--- script.js
+++ script.js
@@ -25810,6 +25810,7 @@ For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur 
 
 More examples in the test cases. 
 */
+/*
 function repeats(arr) {
   let ht = {};
   let res = [];
@@ -25827,9 +25828,45 @@ function repeats(arr) {
 }
 
 console.log(repeats([4, 5, 7, 5, 4, 8]));
+*/
 
-// for (const value in ht) {
-//   if (ht[value] === 1) {
-//     result.push(parseInt(value));
-//   }
-// }
+// #2
+/*
+You're fed up about changing the version of your software manually. Instead, you will create a little script that will make it for you.
+Exercice
+
+Create a function nextVersion, that will take a string in parameter, and will return a string containing the next version number.
+
+For example:
+
+Current           ->  Next version
+"1.2.3"           ->  "1.2.4"
+"0.9.9"           ->  "1.0.0"
+"1"               ->  "2"
+"1.2.3.4.5.6.7.8" ->  "1.2.3.4.5.6.7.9"
+"9.9"             ->  "10.0"
+
+Rules
+
+All numbers, except the first one, must be lower than 10: if there are, you have to set them to 0 and increment the next number in sequence.
+
+You can assume all tests inputs to be valid.
+*/
+function nextVersion(ver) {
+  var arr = ver.split(".").map((el) => parseInt(el));
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] + 1 == 10 && i != 0) {
+      arr[i] = 0;
+    } else {
+      arr[i]++;
+      break;
+    }
+  }
+  return arr.join(".");
+}
+
+console.log(nextVersion("1.2.3"));
+console.log(nextVersion("0.9.9"));
+console.log(nextVersion("1.2.3.4.5.6.7.8"));
+console.log(nextVersion("1"));
+console.log(nextVersion("9.9"));
